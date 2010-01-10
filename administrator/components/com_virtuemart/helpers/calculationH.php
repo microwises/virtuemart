ac3699cb847f41a110a5d1060b311971abf7173d
Milbo <Milbo@87cc427b-4a1e-0410-b729-b2260ec0f063>
2010-01-10 20:05:00 +0000

    added a lot documentation in calculationH.php
    git-svn-id: https://dev.virtuemart.net/svn/virtuemart/trunk/virtuemart@2157 87cc427b-4a1e-0410-b729-b2260ec0f063

--- administrator/components/com_virtuemart/helpers/calculationH.php
+++ administrator/components/com_virtuemart/helpers/calculationH.php
@@ -31,6 +31,11 @@ class calculationHelper{
 //	public $salesPriceCurrency;
 //	public $discountAmount;
 	
+	/** Constructor,... sets the actual date and current currency
+	 * 
+	 * @copyright Copyright (c) 2009 VirtueMart Team. All rights reserved.
+	 * @author Max Milbers
+	 */
 	function __construct(){
 		$this->_db = &JFactory::getDBO();
 		$jnow		=& JFactory::getDate();
@@ -39,48 +44,23 @@ class calculationHelper{
 		$this -> _currency 		= JRequest::getVar('currency');
 	}
 	
-	function getCheckoutPrices($productIds,$cartVendorId=1){
-		$pricesPerId = array();
-		$prices = array();
-		$resultWithTax=0.0;
-		foreach ($productIds as $productId){
-//			echo '$productId '.$productId;
-			if (!array_key_exists($productId,$pricesPerId)){
-				$pricesPerId[$productId] = $this -> getProductPrices($productId);
-//				echo '$productId Calculated '.$productId;
-			}
-			$prices[] = $pricesPerId[$productId];
-			$resultWithTax = $resultWithTax + $pricesPerId[$productId]['salesPrice'];
-			$resultWithOutTax = $resultWithOutTax + $pricesPerId[$productId]['basePrice'];
-		}
-//		echo print_r($prices);
-		echo '<br />';
-		for ($x = 0; $x < sizeof($prices); ++$x){
-			echo "key: ".key($prices)."  value: ".current($prices)."<br />";
-			$steps = current($prices);
-			for ($y = 0; $y < sizeof($steps); ++$y){
-				echo "   key: ".key($steps)."  value: ".current($steps)."<br />";
-				next($steps);
-			}
-			next($prices);
-		}
-		echo '<br />';
-		$dBTaxRules= $this->gatherEffectingRulesForBill('DBTaxBill');
-		$cBRules = $this->gatherEffectingRulesForCoupon();
-		$taxRules  = $this->gatherEffectingRulesForBill('TaxBill');
-		$dATaxRules= $this->gatherEffectingRulesForBill('DATaxBill');
-		$cBRules = $this->gatherEffectingRulesForCoupon();
-		
-		$discountBeforeTax = $this->roundDisplay($this -> executeCalculation($dBTaxRules, $resultWithTax));
-		$discountWithTax = $this->roundDisplay($this -> executeCalculation($taxRules, $discountBeforeTax));
-		$discountAfterTax = $this->roundDisplay($this -> executeCalculation($dATaxRules, $discountWithTax));
-		echo '$basePrice: '.$resultWithTax.'<br />';
-		echo '$discountBeforeTax: '.$discountBeforeTax.'<br />';
-		echo '$discountWithTax: '.$discountWithTax.'<br />';
-		echo '$discountAfterTax: '.$discountAfterTax.'<br />';
-	}
-	
-	/** function to start the calculation, here it is the product
+	/** function to start the calculation, here it is for the product
+	 * 
+	 * The function first gathers the information of the product (maybe better done with using the model)
+	 * After that the function gatherEffectingRulesForProductPrice writes the queries and gets the ids of the rules which affect the product
+	 * The function executeCalculation makes the actual calculation according to the rules 
+	 * 
+	 * @copyright Copyright (c) 2009 VirtueMart Team. All rights reserved.
+	 * @author Max Milbers
+	 * @param int $productId 	The Id of the product
+	 * @param int $catIds 		When the category is already determined, then it makes sense to pass it, if not the function does it for you
+	 * @return int $prices		An array of the prices
+	 * 							'basePrice'  		basePrice calculated in the shopcurrency
+	 * 							'basePriceWithTax'	basePrice with Tax
+	 * 							'discountedPrice'	before Tax
+	 * 							'priceWithoutTax'	price Without Tax but with calculated discounts AFTER Tax. So it just shows how much the shopper saves, regardless which kind of tax
+	 * 							'discountAmount'	he "you save X money"
+	 * 							'salesPrice'		The final price, with all kind of discounts and Tax, except stuff that is only in the checkout
 	 * 
 	 */
 	function getProductPrices($productId,$catIds=0){
@@ -139,19 +119,80 @@ class calculationHelper{
 		return $prices;
 	}
 	
-	function convertCurrencyToShopDefault($currency, $price){
-		if(empty($currency)){
-			return $price;
+	/** function to start the calculation, here it is for the invoice in the checkout
+	 * This function is partly implemented !
+	 * 
+	 * The function calls getProductPrices for every product except it is already known (maybe changed and adjusted with product amount value
+	 * The single prices gets added in an array and already summed up.
+	 * 
+	 * Then simular to getProductPrices first the effecting rules are determined and calculated.
+	 * Ah function to determine the coupon that effects the calculation is already implemented. But not completly in the calculation.
+	 * 
+	 * @copyright Copyright (c) 2009 VirtueMart Team. All rights reserved.
+	 * @author Max Milbers
+	 * @param int $productIds 	The Ids of the products
+	 * @param int $cartVendorId The Owner of the cart, this can be ignored in vm1.5
+	 * @return int $prices		An array of the prices
+	 * 							'resultWithOutTax'	The summed up baseprice of all products
+	 * 							'resultWithTax'  	The final price of all products with their tax, discount and so on
+	 * 							'discountBeforeTax'	discounted price without tax which affects only the checkout (the tax of the products is in it)
+	 * 							'discountWithTax'	discounted price taxed
+	 * 							'discountAfterTax'	final result
+	 * 
+	 */
+	function getCheckoutPrices($productIds,$cartVendorId=1){
+		$pricesPerId = array();
+		$prices = array();
+		$resultWithTax=0.0;
+		foreach ($productIds as $productId){
+//			echo '$productId '.$productId;
+			if (!array_key_exists($productId,$pricesPerId)){
+				$pricesPerId[$productId] = $this -> getProductPrices($productId);
+//				echo '$productId Calculated '.$productId;
+			}
+			$prices[] = $pricesPerId[$productId];
+			$resultWithTax = $resultWithTax + $pricesPerId[$productId]['salesPrice'];
+			$resultWithOutTax = $resultWithOutTax + $pricesPerId[$productId]['basePrice'];
 		}
-		if(empty($this ->_currency)){
-			$this -> _currency 		= JRequest::getVar('currency');
+//		echo print_r($prices);
+		echo '<br />';
+		for ($x = 0; $x < sizeof($prices); ++$x){
+			echo "key: ".key($prices)."  value: ".current($prices)."<br />";
+			$steps = current($prices);
+			for ($y = 0; $y < sizeof($steps); ++$y){
+				echo "   key: ".key($steps)."  value: ".current($steps)."<br />";
+				next($steps);
+			}
+			next($prices);
 		}
-//		if(!strcmp($this->vendorCurrency, $currency)){
-			$price = $this ->_currency->convert( $price, $currency,$this->vendorCurrency);
-//		}
-		return $price;
+		echo '<br />';
+		$dBTaxRules= $this->gatherEffectingRulesForBill('DBTaxBill');
+		$cBRules = $this->gatherEffectingRulesForCoupon();
+		$taxRules  = $this->gatherEffectingRulesForBill('TaxBill');
+		$dATaxRules= $this->gatherEffectingRulesForBill('DATaxBill');
+		$cBRules = $this->gatherEffectingRulesForCoupon();
+		
+		$discountBeforeTax = $this->roundDisplay($this -> executeCalculation($dBTaxRules, $resultWithTax));
+		$discountWithTax = $this->roundDisplay($this -> executeCalculation($taxRules, $discountBeforeTax));
+		$discountAfterTax = $this->roundDisplay($this -> executeCalculation($dATaxRules, $discountWithTax));
+		echo '$resultWithOutTax: '.$resultWithOutTax.'<br />';
+		echo '$basePrice: '.$resultWithTax.'<br />';
+		echo '$discountBeforeTax: '.$discountBeforeTax.'<br />';
+		echo '$discountWithTax: '.$discountWithTax.'<br />';
+		echo '$discountAfterTax: '.$discountAfterTax.'<br />';
 	}
 
+	/**
+	 * Function to execute the calculation of the gathered rules Ids.
+	 * 
+	 * 
+	 * 
+	 * @copyright Copyright (c) 2009 VirtueMart Team. All rights reserved.
+	 * @author Max Milbers
+	 * @param 		$rules 		The Ids of the products
+	 * @param 		$price 		The input price, if no rule is affecting the price just gets returned
+	 * @return int 	$price  	the endprice	
+	 */
 	function executeCalculation($rules, $price){
 		if(empty($rules))return $price;
 		$rulesEffSorted = $this -> record_sort($rules, 'ordering');
@@ -164,7 +205,14 @@ class calculationHelper{
 		return $price;
 	}
 	
-	
+	/**
+	 * Gathers the effecting coupons for the calculation
+	 * 
+	 * @copyright Copyright (c) 2009 VirtueMart Team. All rights reserved.
+	 * @author Max Milbers
+	 * @param 	$code 	The Id of the coupon
+	 * @return 	$rules 	ids of the coupons
+	 */
 	function gatherEffectingRulesForCoupon($code=0){
 		if (empty($code)) return;
 		$couponCodesQuery = $this -> writeRulePartEffectingQuery($code,'coupon_code');
@@ -177,6 +225,15 @@ class calculationHelper{
 		return $rules;
 	}
 
+	/**
+	 * Gathers the effecting rules for the calculation of the bill
+	 * 
+	 * @copyright Copyright (c) 2009 VirtueMart Team. All rights reserved.
+	 * @author Max Milbers
+	 * @param	$entrypoint
+	 * @param	$cartVendorId
+	 * @return $rules The rules that effects the Bill as Ids
+	 */
 	function gatherEffectingRulesForBill($entrypoint, $cartVendorId=1){
 
 //		$cats = $this -> writeRulePartEffectingQuery($this->_cats,'calc_categories');
@@ -205,14 +262,18 @@ class calculationHelper{
 	}
 
 	/**
-	 * Gatheres the rules which affects
-	 * The entrypoint how it should behave. Valid values should be 
+	 * Gatheres the rules which affects the product.
 	 * 
-	 * Profit (Commission is a profit rule that is shared, maybe we remove shared and make a new entrypoint called profit)
-	 * DBTax (Discount for wares, coupons)
-	 * Tax
-	 * DATax (Discount on money)
-	 * Duty
+	 * 
+	 * @copyright Copyright (c) 2009 VirtueMart Team. All rights reserved.
+	 * @author Max Milbers
+	 * @param	$entrypoint The entrypoint how it should behave. Valid values should be 
+	 * 						Profit (Commission is a profit rule that is shared, maybe we remove shared and make a new entrypoint called profit)
+	 * 						DBTax (Discount for wares, coupons)
+	 * 						Tax
+	 * 						DATax (Discount on money)
+	 * 						Duty
+	 * @return	$rules The rules that effects the product as Ids
 	 */ 
 	function gatherEffectingRulesForProductPrice($entrypoint){
 
@@ -241,6 +302,17 @@ class calculationHelper{
 		return $rules;
 	}
 	
+	/**
+	 * This function just writes the query for gatherEffectingRulesForProductPrice
+	 * When a condition is not set, it is handled like a set condition that affects it. So the users have only to add a value 
+	 * for the conditions they want to (You dont need to enter a start or end date when the rule should count everytime).
+	 * 
+	 * @copyright Copyright (c) 2009 VirtueMart Team. All rights reserved.
+	 * @author Max Milbers
+	 * @param $data		the ids of the rule, for exampel the ids of the categories that affect the rule
+	 * @param $field	the name of the field in the db, for exampel calc_categories to write a rule that asks for the field calc_categories
+	 * @return $q		The query
+	 */
 	function writeRulePartEffectingQuery($data,$field){
 		$q='';
 		if(!empty($data)){
@@ -259,6 +331,12 @@ class calculationHelper{
 	 * maybe with function that works like operators, the easier it will be to make more complex disount/commission/profit formulas
 	 * progressive, nonprogressive and so on.
 	 * 
+	 * @copyright Copyright (c) 2009 VirtueMart Team. All rights reserved.
+	 * @author Max Milbers
+	 * @param 	$mathop 	String reprasentation of the mathematical operation
+	 * @param	$value 		The value that affects the price
+	 * @param 	$currency	the currency which should be used
+	 * @param	$price		The price to calculate
 	 */
 	function interpreteMathOp($mathop,$value,$currency, $price){
 
@@ -290,6 +368,19 @@ class calculationHelper{
 	}
 
 
+	function convertCurrencyToShopDefault($currency, $price){
+		if(empty($currency)){
+			return $price;
+		}
+		if(empty($this ->_currency)){
+			$this -> _currency 		= JRequest::getVar('currency');
+		}
+//		if(!strcmp($this->vendorCurrency, $currency)){
+			$price = $this ->_currency->convert( $price, $currency,$this->vendorCurrency);
+//		}
+		return $price;
+	}
+	
 	/**
 	 * Standard round function, we round every number with 6 fractionnumbers
 	 * We need at least 4 to calculate something like 9.25% => 0.0925
@@ -314,7 +405,7 @@ class calculationHelper{
 	/** Sorts indexed 2D array by a specified sub array key
 	 * 
 	 * Copyright richard at happymango dot me dot uk
-	 * 
+	 * @author Max Milbers
 	 */
 	 
 	function record_sort($records, $field, $reverse=false){	
