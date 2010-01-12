ef67736f28265add017c69dc6ff62d667eeaa61c
bass28 <bass28@87cc427b-4a1e-0410-b729-b2260ec0f063>
2010-01-12 23:49:47 +0000

    Work on update view
    git-svn-id: https://dev.virtuemart.net/svn/virtuemart/trunk/virtuemart@2168 87cc427b-4a1e-0410-b729-b2260ec0f063

--- administrator/components/com_virtuemart/views/updatesMigration/tmpl/default_tools.php
+++ administrator/components/com_virtuemart/views/updatesMigration/tmpl/default_tools.php
@@ -1,30 +1,28 @@
 <?php 
 defined('_JEXEC') or die('Restricted access'); 
 ?>
-<table>	
+<table cellpadding="10">
     <tr>
 	<td align="center">
 	    <a onclick="alert('Please don\'t interrupt the next Step! \n It is essential for running VirtueMart.');" href="<?php echo JROUTE::_('index.php?option=com_virtuemart&view=updatesMigration&task=installSampleData'); ?>">
 		<img src="components/com_virtuemart/assets/images/icon_48/vm_install_48.png">
 	    </a>
 	    <br />
-	    Install Sample Data
+	    Install<br />Sample Data
 	</td>
 	<td align="center">
 	    <a href="<?php echo JROUTE::_('index.php?option=com_virtuemart&view=updatesMigration&task=deleteVmData'); ?>">
 		<img src="components/com_virtuemart/assets/images/icon_48/vm_trash_48.png">
 	    </a>
 	    <br />
-	    Remove Virtuemart Data
+	    Remove<br />Virtuemart Data
 	</td>
-    </tr>
-    <tr>
 	<td align="center">
 	    <a href="<?php echo JROUTE::_('index.php?option=com_virtuemart&view=updatesMigration&task=deleteVmTables'); ?>">
 		<img src="components/com_virtuemart/assets/images/icon_48/vm_trash_48.png">
 	    </a>
 	    <br />
-	    Remove all Virtuemart Tables
+	    Remove<br />Virtuemart Tables
 	</td>
 	<td align="center">
 	    &nbsp;
