caf788ee9c36d8ff72abea7b2f0d6944bf4861e8
bass28 <bass28@87cc427b-4a1e-0410-b729-b2260ec0f063>
2010-01-15 02:35:01 +0000

    New store helper done.
    git-svn-id: https://dev.virtuemart.net/svn/virtuemart/trunk/virtuemart@2210 87cc427b-4a1e-0410-b729-b2260ec0f063

--- administrator/components/com_virtuemart/models/store.php
+++ administrator/components/com_virtuemart/models/store.php
@@ -21,7 +21,7 @@ jimport( 'joomla.application.component.model');
  * @author RickG
  */
 class VirtueMartModelStore extends JModel {
-	
+
     /** @var integer Primary key (vendor_id) */
     private $_id;
     /** @var objectlist store data */
@@ -40,20 +40,20 @@ class VirtueMartModelStore extends JModel {
      * @author RickG
      */
     public function __construct() {
-		parent::__construct();
-	
-		// Get the pagination request variables
-		$mainframe = JFactory::getApplication() ;
-		$limit = $mainframe->getUserStateFromRequest('global.list.limit', 'limit', $mainframe->getCfg('list_limit'), 'int');
-		$limitstart = $mainframe->getUserStateFromRequest(JRequest::getVar('option').'.limitstart', 'limitstart', 0, 'int');
-	
-		// Set the state pagination variables
-		$this->setState('limit', $limit);
-		$this->setState('limitstart', $limitstart);
-	
-		// Get the store id or array of ids.
-		$idArray = JRequest::getVar('cid',  0, '', 'array');
-		$this->setId((int)$idArray[0]);
+	parent::__construct();
+
+	// Get the pagination request variables
+	$mainframe = JFactory::getApplication() ;
+	$limit = $mainframe->getUserStateFromRequest('global.list.limit', 'limit', $mainframe->getCfg('list_limit'), 'int');
+	$limitstart = $mainframe->getUserStateFromRequest(JRequest::getVar('option').'.limitstart', 'limitstart', 0, 'int');
+
+	// Set the state pagination variables
+	$this->setState('limit', $limit);
+	$this->setState('limitstart', $limitstart);
+
+	// Get the store id or array of ids.
+	$idArray = JRequest::getVar('cid',  0, '', 'array');
+	$this->setId((int)$idArray[0]);
     }
 
 
@@ -63,8 +63,8 @@ class VirtueMartModelStore extends JModel {
      * @author RickG
      */
     public function setId($id) {
-		$this->_id = $id;
-		$this->_data = null;
+	$this->_id = $id;
+	$this->_data = null;
     }
 
 
@@ -75,11 +75,11 @@ class VirtueMartModelStore extends JModel {
      * @return JPagination Pagination for the current list of stores
      */
     public function getPagination() {
-		if (empty($this->_pagination)) {
-		    jimport('joomla.html.pagination');
-		    $this->_pagination = new JPagination($this->_getTotal(), $this->getState('limitstart'), $this->getState('limit'));
-		}
-		return $this->_pagination;
+	if (empty($this->_pagination)) {
+	    jimport('joomla.html.pagination');
+	    $this->_pagination = new JPagination($this->_getTotal(), $this->getState('limitstart'), $this->getState('limit'));
+	}
+	return $this->_pagination;
     }
 
 
@@ -90,11 +90,11 @@ class VirtueMartModelStore extends JModel {
      * @return int Total number of stores in the database
      */
     public function _getTotal() {
-		if (empty($this->_total)) {
-		    $query = 'SELECT `vendor_id` FROM `#__vm_vendor`';
-		    $this->_total = $this->_getListCount($query);
-		}
-		return $this->_total;
+	if (empty($this->_total)) {
+	    $query = 'SELECT `vendor_id` FROM `#__vm_vendor`';
+	    $this->_total = $this->_getListCount($query);
+	}
+	return $this->_total;
     }
 
 
@@ -105,8 +105,8 @@ class VirtueMartModelStore extends JModel {
      * @return int Total number of store in the database
      */
     public function getTotalNbrOfStores() {
-		$this->_getTotal();
-		return $this->_total;
+	$this->_getTotal();
+	return $this->_total;
     }
 
 
@@ -117,15 +117,15 @@ class VirtueMartModelStore extends JModel {
      * @return int 0 if there are more than 1 store, vendor_id if only one store exists
      */
     public function getIdOfOnlyStore() {
-		if ($this->_total == 1) {
-		    $db = JFactory::getDBO();
-		    $query = 'SELECT `vendor_id` FROM `#__vm_vendor`';
-		    $db->setQuery($query);
-		    return $db->loadResult();
-		}
-		else {
-		    return 0;
-		}
+	if ($this->_total == 1) {
+	    $db = JFactory::getDBO();
+	    $query = 'SELECT `vendor_id` FROM `#__vm_vendor`';
+	    $db->setQuery($query);
+	    return $db->loadResult();
+	}
+	else {
+	    return 0;
+	}
     }
 
 
@@ -138,33 +138,33 @@ class VirtueMartModelStore extends JModel {
      * @author RickG
      */
     public function getStore() {
-		if (empty($this->_data)) {
-		    // Get vendor table data
-		    $vendorTable = $this->getTable('vendor');
-		    $vendorTable->load((int)$this->_id);
-		    $this->_data = $vendorTable;
-		    // Convert ; seperated string into array
-		    $this->_data->vendor_accepted_currencies = explode(',', $this->_data->vendor_accepted_currencies);
-	
-		    $query = "SELECT user_id FROM #__vm_auth_user_vendor ";
-		    $query .= "WHERE vendor_id = '". $this->_id ."'";
-		    $this->_db->setQuery($query);
-		    $userVendor = $this->_db->loadObject();
-	
-		    // Get user_info table data
-		    $userId = (isset($userVendor->user_id) ? $userVendor->user_id : 0);
-		    $userInfoTable = $this->getTable('user_info');
-		    $userInfoTable->load((int)$userId);
-		    $this->_data->userInfo = $userInfoTable;
-		}
-	
-		if (!$this->_data) {
-		    $this->_data = new stdClass();
-		    $this->_id = 0;
-		    $this->_data = null;
-		}
-		
-		return $this->_data;
+	if (empty($this->_data)) {
+	    // Get vendor table data
+	    $vendorTable = $this->getTable('vendor');
+	    $vendorTable->load((int)$this->_id);
+	    $this->_data = $vendorTable;
+	    // Convert ; seperated string into array
+	    $this->_data->vendor_accepted_currencies = explode(',', $this->_data->vendor_accepted_currencies);
+
+	    $query = "SELECT user_id FROM #__vm_auth_user_vendor ";
+	    $query .= "WHERE vendor_id = '". $this->_id ."'";
+	    $this->_db->setQuery($query);
+	    $userVendor = $this->_db->loadObject();
+
+	    // Get user_info table data
+	    $userId = (isset($userVendor->user_id) ? $userVendor->user_id : 0);
+	    $userInfoTable = $this->getTable('user_info');
+	    $userInfoTable->load((int)$userId);
+	    $this->_data->userInfo = $userInfoTable;
+	}
+
+	if (!$this->_data) {
+	    $this->_data = new stdClass();
+	    $this->_id = 0;
+	    $this->_data = null;
+	}
+
+	return $this->_data;
     }
 
 
@@ -175,37 +175,37 @@ class VirtueMartModelStore extends JModel {
      * @return boolean True is the save was successful, false otherwise.
      */
     public function store() {
-		$table = $this->getTable('vendor');
-	
-		$data = JRequest::get('post');
-		// Store multiple selectlist entries as a ; seperated string
-	        if (key_exists('vendor_accepted_currencies', $data) && is_array($data['vendor_accepted_currencies'])) {
-	                $data['vendor_accepted_currencies'] = implode(',', $data['vendor_accepted_currencies']);
-	        }
-		// Store multiple selectlist entries as a | seperated string
-	        if (key_exists('vendor_currency_display_style', $data) && is_array($data['vendor_currency_display_style'])) {
-	                $data['vendor_currency_display_style'] = implode('|', $data['vendor_currency_display_style']);
-	        }
-	
-		// Bind the form fields to the vendor table
-		if (!$table->bind($data)) {
-		    $this->setError($table->getError());
-		    return false;
-		}
-	
-		// Make sure the vendor record is valid
-		if (!$table->check()) {
-		    $this->setError($table->getError());
-		    return false;
-		}
-	
-		// Save the vendor to the database
-		if (!$table->store()) {
-		    $this->setError($table->getError());
-		    return false;
-		}
-		
-		return $this->storeUserInfo($data);
+	$table = $this->getTable('vendor');
+
+	$data = JRequest::get('post');
+	// Store multiple selectlist entries as a ; seperated string
+	if (key_exists('vendor_accepted_currencies', $data) && is_array($data['vendor_accepted_currencies'])) {
+	    $data['vendor_accepted_currencies'] = implode(',', $data['vendor_accepted_currencies']);
+	}
+	// Store multiple selectlist entries as a | seperated string
+	if (key_exists('vendor_currency_display_style', $data) && is_array($data['vendor_currency_display_style'])) {
+	    $data['vendor_currency_display_style'] = implode('|', $data['vendor_currency_display_style']);
+	}
+
+	// Bind the form fields to the vendor table
+	if (!$table->bind($data)) {
+	    $this->setError($table->getError());
+	    return false;
+	}
+
+	// Make sure the vendor record is valid
+	if (!$table->check()) {
+	    $this->setError($table->getError());
+	    return false;
+	}
+
+	// Save the vendor to the database
+	if (!$table->store()) {
+	    $this->setError($table->getError());
+	    return false;
+	}
+
+	return $this->storeUserInfo($data);
     }
 
 
@@ -216,27 +216,27 @@ class VirtueMartModelStore extends JModel {
      * @return boolean True is the save was successful, false otherwise.
      */
     public function storeUserInfo($data) {
-		$table = $this->getTable('user_info');
-	
-		// Bind the form fields to the user info table
-		if (!$table->bind($data)) {
-		    $this->setError($table->getError());
-		    return false;
-		}
-	
-		// Make sure the user info record is valid
-		if (!$table->check()) {
-		    $this->setError($table->getError());
-		    return false;
-		}
-	
-		// Save the user info to the database
-		if (!$table->store()) {
-		    $this->setError($table->getError());
-		    return false;
-		}
-	
-		return true;
+	$table = $this->getTable('user_info');
+
+	// Bind the form fields to the user info table
+	if (!$table->bind($data)) {
+	    $this->setError($table->getError());
+	    return false;
+	}
+
+	// Make sure the user info record is valid
+	if (!$table->check()) {
+	    $this->setError($table->getError());
+	    return false;
+	}
+
+	// Save the user info to the database
+	if (!$table->store()) {
+	    $this->setError($table->getError());
+	    return false;
+	}
+
+	return true;
     }
 
 
@@ -247,17 +247,17 @@ class VirtueMartModelStore extends JModel {
      * @return boolean True is the delete was successful, false otherwise.
      */
     public function delete() {
-		$creditcardIds = JRequest::getVar('cid',  0, '', 'array');
-		$table = $this->getTable('vendor');
-	
-		foreach($vendorIds as $vendorId) {
-		    if (!$table->delete($vendorId)) {
-			$this->setError($table->getError());
-			return false;
-		    }
-		}
-	
-		return true;
+	$creditcardIds = JRequest::getVar('cid',  0, '', 'array');
+	$table = $this->getTable('vendor');
+
+	foreach($vendorIds as $vendorId) {
+	    if (!$table->delete($vendorId)) {
+		$this->setError($table->getError());
+		return false;
+	    }
+	}
+
+	return true;
     }
 
 
@@ -268,9 +268,9 @@ class VirtueMartModelStore extends JModel {
      * @return object List of store objects
      */
     public function getStores() {
-		$query = 'SELECT * FROM `#__vm_vendor` ';
-		$query .= 'ORDER BY `#__vm_vendor`.`vendor_id`';
-		$this->_data = $this->_getList($query, $this->getState('limitstart'), $this->getState('limit'));
-		return $this->_data;
+	$query = 'SELECT * FROM `#__vm_vendor` ';
+	$query .= 'ORDER BY `#__vm_vendor`.`vendor_id`';
+	$this->_data = $this->_getList($query, $this->getState('limitstart'), $this->getState('limit'));
+	return $this->_data;
     }
 }
