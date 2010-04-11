2b0b40bb3e4d2a431c0fc68499fb4f63415450c2
rolandd <rolandd@87cc427b-4a1e-0410-b729-b2260ec0f063>
2010-04-11 22:00:11 +0000

    + Add skeleton for checkout
    git-svn-id: https://dev.virtuemart.net/svn/virtuemart/trunk/virtuemart@2354 87cc427b-4a1e-0410-b729-b2260ec0f063

---
+++ components/com_virtuemart/models/checkout.php
@@ -0,0 +1,37 @@
+<?php
+/**
+*
+* Category model for the checkout
+*
+* @package	VirtueMart
+* @subpackage Checkout
+* @author RolandD
+* @link http://www.virtuemart.net
+* @copyright Copyright (c) 2004 - 2010 VirtueMart Team. All rights reserved.
+* @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.php
+* VirtueMart is free software. This version may have been modified pursuant
+* to the GNU General Public License, and as distributed it includes or
+* is derivative of works licensed under the GNU General Public License or
+* other free or open source software licenses.
+* @version $Id: cart.php 2343 2010-03-31 20:03:37Z milbo $
+*/
+ 
+// Check to ensure this file is included in Joomla!
+defined('_JEXEC') or die('Restricted access');
+
+// Load the model framework
+jimport( 'joomla.application.component.model');
+
+/**
+* Default model class for the checkout
+*
+* @package	VirtueMart
+* @subpackage Checkout
+* @author RolandD
+*
+*/
+class VirtueMartModelCheckout extends JModel {
+	
+	
+}
+?>
