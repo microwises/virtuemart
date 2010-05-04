ec15c56da6f4faaaab5b7ef8e3b90dee8dfb9d17
wicksj <wicksj@87cc427b-4a1e-0410-b729-b2260ec0f063>
2010-05-04 03:55:13 +0000

    Initial commit of MVC report module for vm 1.5
    git-svn-id: https://dev.virtuemart.net/svn/virtuemart/trunk/virtuemart@2378 87cc427b-4a1e-0410-b729-b2260ec0f063

---
+++ administrator/components/com_virtuemart/controllers/report.php
@@ -0,0 +1,56 @@
+<?php
+if( !defined( '_JEXEC' ) ) die( 'Direct Access to '.basename(__FILE__).' is not allowed.' );
+
+/**
+*
+* @version 
+* @package VirtueMart
+* @subpackage Report
+* @copyright Copyright (C) VirtueMart Team - All rights reserved.
+* @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.php
+* VirtueMart is free software. This version may have been modified pursuant
+* to the GNU General Public License, and as distributed it includes or
+* is derivative of works licensed under the GNU General Public License or
+* other free or open source software licenses.
+* See /administrator/components/com_virtuemart/COPYRIGHT.php for copyright notices and details.
+*
+* http://virtuemart.org
+*/
+
+// Load the controller framework
+jimport('joomla.application.component.controller');
+
+/**
+ * Report Controller
+ * 
+ * @package	VirtueMart
+ * @subpackage Report
+ * @author Wicksj
+ */
+class VirtuemartControllerReport extends JController {
+	
+	/**
+	 * Report Controller Constructor
+	 */
+	function __constuct(){
+		parent::__construct();
+		
+		$document = JFactory::getDocument();
+		$viewType = $document->getType();
+		$view = $this->getView('report', $viewType);
+		
+		$model = $this->getModel('report');
+		if(!JError::isError($model)){
+			$view->setModel($model, true);
+		}
+	}
+	
+	/**
+	 * Display Report View
+	 */
+	function display(){
+		parent::display();
+	}
+	
+}
+?>
