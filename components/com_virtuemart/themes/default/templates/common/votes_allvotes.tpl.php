c42ab4c230bbff8eddf5656f4d3caae33c17aa0a
milbo <milbo@87cc427b-4a1e-0410-b729-b2260ec0f063>
2010-07-09 13:24:35 +0000

    Also old vm1.2 files in vm1.5, delete them now
    git-svn-id: https://dev.virtuemart.net/svn/virtuemart/trunk/virtuemart@2476 87cc427b-4a1e-0410-b729-b2260ec0f063

--- components/com_virtuemart/themes/default/templates/common/votes_allvotes.tpl.php
+++
@@ -1,7 +0,0 @@
-<?php if( !defined( '_JEXEC' ) ) die( 'Direct Access to '.basename(__FILE__).' is not allowed.' ); ?>
-
-<!-- The "Average Customer Rating: xxxxX (2 votes) " Part -->
-<span class="contentpagetitle"><?php echo JText::_('VM_CUSTOMER_RATING') ?>:</span>
-<br />
-<img src="<?php echo VM_THEMEURL ?>images/stars/<?php echo $rating ?>.gif" align="middle" border="0" alt="<?php echo $rating ?> stars" />&nbsp;
-<?php echo JText::_('VM_TOTAL_VOTES').": ". $allvotes; ?>
