d1c33f2d13fce2bb177031a750498cf8081df396
Electrocity <Electrocity@87cc427b-4a1e-0410-b729-b2260ec0f063>
2011-02-10 12:51:45 +0000

    Router.php file
    git-svn-id: https://dev.virtuemart.net/svn/virtuemart/trunk/virtuemart@2696 87cc427b-4a1e-0410-b729-b2260ec0f063

--- components/com_virtuemart/router.php
+++ components/com_virtuemart/router.php
@@ -2,10 +2,10 @@
 if(  !defined( '_JEXEC' ) ) die( 'Direct Access to '.basename(__FILE__).' is not allowed.' ); 
 /**
 *
-* @version $Id: 
+* @version $Id: 1.9 beta1
 * @package VirtueMart
 * @subpackage html
-* @copyright Copyright (C) 2010 Max Milbers - All rights reserved.
+* @copyright Copyright (C) 2010 Kohl Patrick - All rights reserved.
 * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.php
 * VirtueMart is free software. This version may have been modified pursuant
 * to the GNU General Public License, and as distributed it includes or
@@ -16,328 +16,176 @@ if(  !defined( '_JEXEC' ) ) die( 'Direct Access to '.basename(__FILE__).' is not
 * http://virtuemart.net
 */
 
-define('VM_SHOP_BROWSE','category');
-define('VM_SHOP_FEED','feed');
-define('VM_PRODUCT_DETAILS','details');
-define('VM_PRODUCT_ENQUIRY','enquiry');
-define('VM_CHECKOUT_INDEX','checkout');
-define('VM_ADVANCE_SEARCH','search');
+
+
+/* views are virtuemart\user\state\productdetails\orders\category\cart\*/
 
 function virtuemartBuildRoute(&$query)
 {
-	$page = '';
+	$view = '';
 	$segments = array();
 
+	$menu = &JSite::getMenu();
+	if (empty($query['Itemid'])) {
+		$menuItem = &$menu->getActive();
+
+	} else {
+		$menuItem = &$menu->getItem($query['Itemid']);
+	}
+
+	$menuView	= (empty($menuItem->query['view'])) ? null : $menuItem->query['view'];
+	$menuCatid	= (empty($menuItem->query['category_id'])) ? 0 : $menuItem->query['category_id'];
+	$menuProdId	= (empty($menuItem->query['product_id'])) ? null : $menuItem->query['product_id'];
+	$menuComponent = $menuItem->component ;
+	if (isset($menuItem->id)) $menuId = $menuItem->id ;
+
+	
+
 	if(isset($query['view'])){
-		$page = $query['view'];
+		$view = $query['view'];
 		unset($query['view']);
 	}
 
-	switch ($page) {		
-		// Shop browse/catgory page 
-		case 'category';	
-			if(isset($query['category_id'])){
-//				$segments[] = VM_SHOP_BROWSE;
-				$segments[] = $query['category_id'];				
-				$segments[] = getCategoryName($query['category_id']);		
-				unset($query['category_id']);
-			}else{
-				$segments[] = "products";
-				unset($query['category']);	
+/* Find if a menu is set with a parent categorie 
+    add the Id of joomla menu  
+    fix the module routes outside of virtuemart */
+	static $firsttime=true ;
+	static $VirtuemartMenuCat = array();
+	static $VirtuemartMenuVM = array();
+	if ($firsttime) {
+	$firsttime = false ;
+		$app		= JFactory::getApplication();
+		$menus		= $app->getMenu('site');
+		$component	= JComponentHelper::getComponent('com_virtuemart');
+		$items		= $menus->getItems('componentid', $component->id);
+		// set all category and vrituemart root menu id
+		foreach ($items as $item)	{
+			if ( $item->query['view']=='category' && isset( $item->query['category_id'])) {
+				$VirtuemartMenuCat[]  = array_merge( $item->query, array('itemId' => $item->id) );  ;
+				
 			}
-		break;
-
-		// Shop rss feed page 
-		case 'shop.feed';			
-			$segments[] = VM_SHOP_FEED;
-			if(isset($query['category_id'])){
-				$segments[] = $query['category_id'];
-				$segments[] = getCategoryName($query['category_id']);		
-				unset($query['category_id']);
-			}			
-		break;
-
-		// Shop product details page 
-		case 'productdetails';			
-//			$segments[] = VM_PRODUCT_DETAILS;			
-			$product_id_exists = false;
-			if(isset($query['product_id']))	{
-//				$segments[] = $query['product_id'];
-				$product_id_exists = true;
-				$product_id = $query['product_id'];
-				unset($query['product_id']);
+			if ( $item->query['view']=='virtuemart' ) {
+				$VirtuemartMenuVM[]  = array_merge($item->query, array('itemId' => $item->id) ); 
 			}
-			if(isset($query['category_id'])){
-				$segments[] = $query['category_id'];
-				$segments[] = getCategoryName($query['category_id']);		
-				unset($query['category_id']);
-			}
-//			if(isset($query['pop']) )	{
-//				unset($query['pop']);				
-//			}
-			if($product_id_exists)	{
-				$segments[] = getProductName($product_id);
-			}
-		break;
 			
-		// Shop ASK A QUESTION ABOUT THIS PRODUCT 
-		case 'shop.ask';
-			$segments[] =VM_PRODUCT_ENQUIRY;				
-			if(isset($query['category_id']))	{
-				$segments[] = $query['category_id'];
+		}
+
+	}
+
+	// give the 1st Virtuemart menu ID if no categories are set in joomla menu
+	if ($menuComponent != 'com_virtuemart' ) $query['Itemid'] = $VirtuemartMenuVM[0]['itemId'] ;
+	
+	switch ($view) {
+		case 'virtuemart';
+			unset($query['view']);
+		// Shop category view 
+		case 'category';	
+			if(isset( $query['category_id']) && $menuCatid != $query['category_id'] ){
+				// to avoid duplicate categorie if a joomla menu ID is set
+				$ismenu = false ;
+				$treeIds = getCategoryRecurse($query['category_id'],true,$menuCatid) ;
+				foreach ($VirtuemartMenuCat as $menuId) {
+					foreach ($treeIds as $treeId) {
+						if ($menuId['category_id'] == $treeId && $ismenu == false ) {
+							$query['Itemid'] = $menuId['itemId'] ;
+							if ($query['category_id'] ==  $treeId) {
+								$ismenu = true ;
+							
+							}
+						}
+					}
+				}
+				
+				if (!$ismenu) {
+					$segments[] = $query['category_id'];				
+					$segments[] = getCategoryName($query['category_id'], true, $menuCatid );
+				}
+				unset($query['category_id']);
+			} else {
 				unset($query['category_id']);
 			}
-			if(isset($query['product_id']))	{
+		break;
+		// Shop product details view 
+		case 'productdetails';			
+			$product_id_exists = false;
+			if(isset($query['product_id'])) {
 				$segments[] = $query['product_id'];
 				$product_id_exists = true;
 				$product_id = $query['product_id'];
 				unset($query['product_id']);
 			}
+			if(isset( $query['category_id'])){
+				// to avoid duplicate categorie if a joomla menu ID is set 
+				if ($menuCatid != $query['category_id'] ){
+				$ismenu = false ;
+				$treeIds = getCategoryRecurse($query['category_id'],true,$menuCatid) ;
+				foreach ($VirtuemartMenuCat as $menuId) {
+					foreach ($treeIds as $treeId) {
+						if ($menuId['category_id'] == $treeId && $ismenu == false ) {
+							$query['Itemid'] = $menuId['itemId'] ;
+							if ($query['category_id'] ==  $treeId) {
+								$ismenu = true ;
+							
+							}
+						}
+					}
+				}
+				
+				if (!$ismenu) {
+					$segments[] = $query['category_id'];				
+					$segments[] = getCategoryName($query['category_id'], true, $menuCatid );
+				}
+				unset($query['category_id']);
+				} else {
+					$segments[] = $query['category_id'];
+					unset($query['category_id']);
+				}
+			}
 			if($product_id_exists)	{
 				$segments[] = getProductName($product_id);
 			}
 		break;
+		// sef only view	
+		default ;
+		$segments[] = $view;
+		
 
-		// Checkout Index page			
-		case 'cart';
-            		
-//		$segments[] = VM_CHECKOUT_INDEX;
-//		
-//		if(isset($query['ssl_redirect']))	{
-//			$segments[] = "ssl_redirect";
-//			unset($query['ssl_redirect']);
-//		}
-//		if(isset($query['redirected']))	{
-//			$segments[] = "redirected";
-//			unset($query['redirected']);
-//		}
-		break;		
-
-//		case 'account.billing';
-//			$segments[] ="account-billing";
-//			if(isset($query['next_page']) )	{
-//				$segments[] = "checkout";
-//				unset($query['next_page']);				
-//			}
-//		break;
-//
-//		case 'account.shipto';
-//			$segments[] ="account-shipto";
-//			if(isset($query['next_page']) )	{
-//				$segments[] = "checkout";
-//				unset($query['next_page']);				
-//			}
-//		break;
-//
-//		case 'account.shipping';
-//			$segments[] ="account-shipping";
-//			if(isset($query['next_page']) )	{
-//				$segments[] = "checkout";
-//				unset($query['next_page']);				
-//			}
-//		break;
-//
-//		case 'shop.registration';
-//			$segments[] ="user-registration";
-//		break;
-//		
-//		case 'shop.favourites';
-//			$segments[] ="favourites";
-//		break;
-//
-//		case 'shop.recommend';
-//			$segments[] ="recommend";
-//			if(isset($query['tmpl']) )	{
-//				$segments[] = $query['tmpl'];
-//				unset($query['tmpl']);				
-//			}
-//			if(isset($query['pop']) )	{
-//				$segments[] = $query['pop'];
-//				unset($query['pop']);				
-//			}
-//			if(isset($query['product_id']) ){
-//				$segments[] = $query['product_id'];
-//				$segments[] = getProductName($query['product_id']);
-//				unset($query['product_id']);				
-//			}
-//		break;
-//
-//		case 'shop.tos';
-//			$segments[] ="terms-of-service";
-//		break;
-//
-//		case 'shop.cart';
-//			$segments[] ="cart";
-//		break;
-//
-//		case 'account.index';
-//			$segments[] ="account";
-//		break;
-//
-//		case 'account.order_details';
-//			$segments[] ="order-details";
-//			if(isset($query['order_id']))	{
-//				$segments[] = $query['order_id'];
-//				unset($query['order_id']);
-//			}
-//		break;
-//
-//		case 'shop.waiting_list';
-//			$segments[] ="notify";
-//			if(isset($query['product_id']))	{
-//				$segments[] = $query['product_id'];
-//				$product_id_exists = true;
-//				$product_id = $query['product_id'];
-//				unset($query['product_id']);
-//			}
-//			if($product_id_exists)	{
-//				$segments[] = getProductName($product_id);
-//			}
-//		break;
-//
-//		case 'shop.search';
-//			$segments[] =VM_ADVANCE_SEARCH;			
-//		break;
-//
-//		case 'store.index';
-//			$segments[] = 'administration';			
-//		break;
 	} 
+	// sef the task
+	if (isset($query['task'])) {
+		$segments[] = $query['task'] ;
+		unset($query['task']);
+	}
 	return $segments;
 }
 
 function virtuemartParseRoute($segments)
 {
-
 	$vars = array();
+	$count = count($segments) ;
+	$menu =& JSite::getMenu();
+	$menuItem =& $menu->getActive();
+	$menuCatid	= (empty($menuItem->query['category_id'])) ? 0 : $menuItem->query['category_id'];
 
+	$segments[0]=str_replace(":", "-",$segments[0]);
 	$firstSegment = $segments[0]; 
-	switch($firstSegment){
-		
-		case 'cart':
-			$uri = JFactory::getURI();
-			$redirect  = 1;
-			// Redirect to SSL
-			if($redirect == true) {
-				$uri->setScheme('https');
-				$application->redirect($uri->toString());
-			}
-		break;
-		
-		case VM_SHOP_BROWSE:
-			$vars['page'] = "shop.browse";
-			if(isset($segments[1])){
-				$vars['category_id'] = $segments[1];
-			}
-		break;
 
-		//This is for all products page
-		case 'products':
-			$vars['page'] = "shop.browse";
-			$vars['category'] = "";	
-		break;
-		
-		case VM_SHOP_FEED:
-			$vars['page'] = "shop.feed";
-			if(isset($segments[1])){
-				$vars['category_id'] = $segments[1];
-			}			
-		break;
-
-		case VM_PRODUCT_DETAILS:
-			$vars['page'] = "shop.product_details";			
-			if(isset($segments[1])){
-				$vars['product_id'] = $segments[1];
-			}
-			if(isset($segments[2])){
-				$vars['category_id'] = $segments[2];
-			}			
-		break;
-
-		case VM_PRODUCT_ENQUIRY:
-			$vars['page'] = "shop.ask";
+	if (ctype_digit ($segments[0])) {
+		if (ctype_digit ($segments[1]) ) {
+			$vars['product_id'] = $segments[0];
 			$vars['category_id'] = $segments[1];
-			$vars['product_id'] = $segments[2];
-		break; 
-
-		case VM_CHECKOUT_INDEX:
-			$vars['page'] = "checkout.index";		
-			if(isset($segments[1]) && ($segments[1]=="ssl_redirect")){
-				$vars['ssl_redirect'] = 1;
-				$vars['redirected'] = 1;
-			}
-			if(isset($segments[2]) && ($segments[2]=="redirected")){
-				$vars['redirected'] = 1;
-			}
-		break;
-
-		case 'account:billing':
-			$vars['page'] = "account.billing";
-			if(isset($segments[1])){
-				$vars['next_page'] = "checkout.index";
-			}
-		break;
-
-		case 'account:shipto':
-			$vars['page'] = "account.shipto";
-			if($segments[1]){
-				$vars['next_page'] = "checkout.index";
-			}
-		break;
-
-		case 'account:shipping':
-			$vars['page'] = "account.shipping";
-			if($segments[1]){
-				$vars['next_page'] = "checkout.index";
-			}
-		break;
-
-		case 'favourites';
-			$vars['page'] = "shop.favourites";
-		break;
-
-		case 'recommend';
-			$vars['page'] = "shop.recommend";
-			$vars['pop'] = 1;
-			$vars['product_id'] = $segments[3];
-			$vars['tmpl']= "component";
-		break;
-
-		case 'user:registration';
-			$vars['page'] = "shop.registration";
-		break;
-
-		case 'account':
-			$vars['page'] = "account.index";
-		break; 
-
-		case 'cart':
-			$vars['page'] = "shop.cart";
-		break; 
-
-		case 'order:details':
-			$vars['page'] = "account.order_details";
-			$vars['order_id'] = $segments[1];				
-		break;
-
-		case 'temrs:of:service':
-			$vars['page'] = "shop.tos";				
-		break;
-
-		case 'notify':
-			$vars['page'] = "shop.waiting_list";
-			$vars['product_id'] = $segments[1];				
-		break;
-
-		case VM_ADVANCE_SEARCH:
-			$vars['page'] = "shop.search";					
-		break;
-
-		case 'administration':
-			$vars['page'] = "store.index";
-			$vars['pshop_mode'] = "admin";
-					
-		break;
+			$vars['view'] = 'productdetails';
+		}
+		else {
+			$vars['category_id'] = $segments[0];
+			$vars['view'] = 'category';
+		}
+	return $vars;
+	} else {
+		$vars['view'] = $firstSegment;
+		if ( isset($segments[1]) ) {
+			$vars['task'] = $segments[1] ;
+		}
 	}
 	return $vars;
 }
@@ -345,45 +193,55 @@ function virtuemartParseRoute($segments)
 
 // This function returns category/subcatgory alias string
 
-function getCategoryName($id){
-	
-	$db			= & JFactory::getDBO();
-	$catIdsList = implode( ',', getCategoryRecurse($id,true) );
-	// End of need to change
-	$q = "SELECT GROUP_CONCAT( `category_name`
-			SEPARATOR  '/' ) 
-			FROM `#__vm_category`
-			WHERE `category_id` IN (".$catIdsList.")";
+function getCategoryName($category_id,$catMenuId=0,$menu){
 
-	$db->setQuery($q);
-	$category_name = $db->loadResult();
-	$category_alias = strtolower($category_name);
+	$strings = array();
+	$db = & JFactory::getDBO();
+	$parents_id = getCategoryRecurse($category_id,true,$catMenuId,$menu) ;
+	foreach ($parents_id as $id ) {
+		$q = "SELECT `category_name` as name
+				FROM  `#__vm_category` 
+				WHERE  `category_id`=".$id;
 
-	//Remove following characters
-	$special_chars = array('!','@','#','$','%','*','(',')');
-	foreach($special_chars as $char){
-		$category_alias = str_replace($char,'', $category_alias);
+		$db->setQuery($q);
+		$category = $db->loadResult();
+		$string  = $category;
+		if ( ctype_digit(trim($string)) ){
+			return trim($string);
+		}
+		else {	
+			// accented chars converted
+			$accents = '/&([A-Za-z]{1,2})(grave|acute|circ|cedil|uml|lig);/';
+			$string_encoded = htmlentities($string,ENT_NOQUOTES,'UTF-8');
+			$string = preg_replace($accents,'$1',$string_encoded);
+			
+			// clean out the rest
+			$replace = array('([\40])','/&nbsp/','/&amp/','/\//','([^a-zA-Z0-9-/])','/\-+/');
+			$with = array('-','-','-','-','-','-');
+			$string = preg_replace($replace,$with,$string);
+			
+		}
+		$strings[] = $string;
 	}
+	
+	return strtolower(implode ('/', $strings ) );
 
-	$category_alias = str_replace(' ','-', $category_alias);
-	$category_alias = str_replace('  ','-', $category_alias);
-	return $category_alias;
 }
 
-function getCategoryRecurse($category_id,$first ) {
+function getCategoryRecurse($category_id,$first,$catMenuId ) {
 	static $idsArr = array();
 	if($first) {
 		$idsArr = array();
 	}
-	//
+
 	$db			= & JFactory::getDBO();	
 	$q = "SELECT `category_child_id` AS `child`, `category_parent_id` AS `parent`
 			FROM  #__vm_category_xref AS `xref`
 			WHERE `xref`.`category_child_id`= ".$category_id;
 	$db->setQuery($q);
 	$ids = $db->loadObject();
-	if($ids->parent != 0) {
-		getCategoryRecurse($ids->parent,false);
+	if($ids->parent != 0 and $catMenuId != $category_id and $catMenuId != $ids->parent) {
+		getCategoryRecurse($ids->parent,false,$catMenuId);
 	} 
 	$idsArr[] = $ids->child;
 	
@@ -402,16 +260,23 @@ function getProductName($id){
 	$db->setQuery($query);
 	// gets product name of item
 	$product_name = $db->loadResult();
-	$product_name = strtolower($product_name);
-
-	//Remove following characters
-	$special_chars = array('!','@','#','$','%','*','(',')');
-	foreach($special_chars as $char){
-		$product_name = str_replace($char,'', $product_name);
-	}
-	$product_name = str_replace(' ','-', $product_name);
-	$product_name = str_replace('  ','-', $product_name);
-	return $product_name;
+		$string  = $product_name;
+		if ( ctype_digit(trim($string)) ){
+			return trim($string);
+		}
+		else {	
+			// accented chars converted
+			$accents = '/&([A-Za-z]{1,2})(grave|acute|circ|cedil|uml|lig);/';
+			$string_encoded = htmlentities($string,ENT_NOQUOTES,'UTF-8');
+			$string = preg_replace($accents,'$1',$string_encoded);
+			
+			// clean out the rest
+			$replace = array('([\40])','/&nbsp/','/&amp/','/\//','([^a-zA-Z0-9-/])','/\-+/');
+			$with = array('-','-','-','-','-','-');
+			$string = preg_replace($replace,$with,$string);
+			
+		}
+	return $string;
 }
 
 // pure php no closing tag
