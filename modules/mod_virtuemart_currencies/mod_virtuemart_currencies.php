f5b753e1c35f380bae95ecd8d115a47d70a05dfe
Electrocity <Electrocity@87cc427b-4a1e-0410-b729-b2260ec0f063>
2011-02-15 03:21:42 +0000

    The Currencies Module. for VM 2.0
    git-svn-id: https://dev.virtuemart.net/svn/virtuemart/trunk/virtuemart@2727 87cc427b-4a1e-0410-b729-b2260ec0f063

--- modules/mod_virtuemart_currencies/mod_virtuemart_currencies.php
+++ modules/mod_virtuemart_currencies/mod_virtuemart_currencies.php
@@ -23,13 +23,30 @@ if( !defined( '_VALID_MOS' ) && !defined( '_JEXEC' ) ) die( 'Direct Access to '.
  * in the cart:
  * 	currency_id: ID of the user selected currenct
  * 	currency_rate: Actual rate compared to the shop currency
- * 
+ * 	
  * Prices in the orders are saved in the shop currency; these fields are required
  * to show the prices to the user in a later stadium.
- */
-// Session variable shopper_currency
- $session =& JFactory::getSession();
-$session->set( 'myvar', 'helloworld' );
-dump ($session, 'my session'); ?>
-	
-	Choisir sa monnaie
+  */
+$currency_id = $mainframe->getUserStateFromRequest( "currency_id", 'currency_id',JRequest::getInt('currency_id', 1) );
+
+$vendorId = JRequest::getInt('vendorid', 1);
+$text_before = $params->get( 'text_before', '');
+// table vm_vendor
+$db = JFactory::getDBO();
+$q = 'SELECT `vendor_accepted_currencies` FROM `#__vm_vendor` WHERE `vendor_id`='.$vendorId;
+$db->setQuery($q);
+$currency_codes = explode(',',$db->loadResult());
+// table vm_currency
+$q = 'SELECT `currency_id`,`currency_code`,`currency_name` FROM `#__vm_currency` WHERE `currency_code` IN ("'.implode('","',$currency_codes).'") and published =1 ORDER BY `currency_name`';
+$db->setQuery($q);
+$currencies = $db->loadObjectList();
+
+$currenciesList = JHTML::_('select.genericlist', $currencies, 'currency_id', 'class="inputbox"', 'currency_id', 'currency_name', $currency_id) ;
+    ?>
+<!-- Currency Selector Module -->
+<?php echo $text_before ?>
+<form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post">
+	<br />
+	<?php echo $currenciesList ?>
+    <input class="button" type="submit" name="submit" value="<?php echo JText::_('Change Currency') ?>" />
+</form>
