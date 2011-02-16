09d0958badbd794c1d4732226c046505c8814e69
impleri <impleri@87cc427b-4a1e-0410-b729-b2260ec0f063>
2011-02-16 11:58:30 +0000

    Small problem with == on isJ15 made it return true if strpos returned null.
    git-svn-id: https://dev.virtuemart.net/svn/virtuemart/trunk/virtuemart@2732 87cc427b-4a1e-0410-b729-b2260ec0f063

--- administrator/components/com_virtuemart/helpers/config.php
+++ administrator/components/com_virtuemart/helpers/config.php
@@ -13,7 +13,7 @@ defined('_JEXEC') or die('Restricted access');
 
 require_once(JPATH_ADMINISTRATOR.DS.'components'.DS.'com_virtuemart'.DS.'version.php');
 
-class VmConfig 
+class VmConfig
 {
 	/**
 	 * Load the configuration values from the database into a session variable.
@@ -21,25 +21,25 @@ class VmConfig
 	 *
 	 * @author RickG
 	 */
-	function loadConfig() {  
+	function loadConfig() {
 		$db = JFactory::getDBO();
 		$query = "SELECT `config` FROM `#__vm_config` WHERE `config_id` = '1'";
 		$db->setQuery($query);
 		$config = $db->loadResult();
-	
+
 		$session = JFactory::getSession();
 		$session->clear('vmconfig');
 		$session->set('vmconfig', $config,'vm');
 	}
-	
-	
+
+
 	/**
 	 * Find the configuration value for a given key
 	 *
 	 * @author RickG
 	 * @param string $key Key name to lookup
 	 * @return Value for the given key name
-	 */	
+	 */
 	function get($key = '', $default='')
 	{
 		$value = '';
@@ -47,49 +47,49 @@ class VmConfig
 			jimport('joomla.html.parameter');
 			$session = JFactory::getSession();
 			$config = $session->get('vmconfig', '','vm');
-			if (!$config) { 
+			if (!$config) {
 				VmConfig::loadConfig();
 				$config = $session->get('vmconfig', '','vm');
 			}
-				
+
 			if ($config) {
 				$params = new JParameter($config);
-				$value = $params->get($key);				
+				$value = $params->get($key);
 			}
 			else {
 			    $params = new JParameter('');
 			    $value = '';
 			}
-			
+
 			if ($value == '') {
 			    $params->set($key, $default);
 			    $value = $default;
 			}
-		}	
-		
+		}
+
 		return $value;
 	}
-	
-	
+
+
 	/**
 	 * Find the currenlty installed version
 	 *
 	 * @author RickG
 	 * @param boolean $includeDevStatus True to include the development status
 	 * @return String of the currently installed version
-	 */	
+	 */
 	function getInstalledVersion($includeDevStatus=false)
 	{
 		// Get the installed version from the wmVersion class.
-		
-		return vmVersion::$RELEASE;	
-	}	
-	
+
+		return vmVersion::$RELEASE;
+	}
+
 	/**
 	 * Compares two "A PHP standardized" version number against the current Joomla! version
 	 * This function needs at least 3 digits, like 1.5.0,
 	 * We can use it like isAtLeastVersion('1.6.0')
-	 * 
+	 *
 	 * This function returns a true if the version is equal or higher
 	 * @return boolean
 	 * @see http://www.php.net/version_compare
@@ -97,12 +97,12 @@ class VmConfig
 	function isAtLeastVersion ( $minimum ) {
 		return (version_compare( JVERSION, $minimum ) == 1);
 	}
-	
+
 	/**
 	 * Return if the used joomla function is j15
 	 */
 	function isJ15(){
-		if(strpos(JVERSION,'1.5')==0) return true; else false;
+		return (strpos(JVERSION,'1.5') === 0);
 	}
 }
 // pure php no closing tag
