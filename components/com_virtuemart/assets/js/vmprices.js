38afbba34d58fb06a74bf46dc1406951a2a843c8
Electrocity <Electrocity@87cc427b-4a1e-0410-b729-b2260ec0f063>
2011-02-18 04:32:41 +0000

    Add to cart SELECT LIST >>Ajax change function added
    git-svn-id: https://dev.virtuemart.net/svn/virtuemart/trunk/virtuemart@2739 87cc427b-4a1e-0410-b729-b2260ec0f063

--- components/com_virtuemart/assets/js/vmprices.js
+++ components/com_virtuemart/assets/js/vmprices.js
@@ -1,44 +1,53 @@
 jQuery(document).ready(function() {
 
-		jQuery("[name='addtocart']").click(function(e) { 
+	jQuery("[name='addtocart']").click(function(e) { 
 		e.preventDefault();
 		sendtocart(jQuery(this).parents("form"));
-		});
-		jQuery("[name='setproducttype']").click(function(e) { 
+	});
+	jQuery("[name='setproducttype']").click(function(e) { 
 		e.preventDefault();
 		setproducttype(jQuery(this).parents("form"),jQuery(this).attr('id'));
-		});
+	});
+	
+	
+	 jQuery("form[id^=addtocartproduct]").find('select').change(function(e) { 
+		//e.preventDefault();
+		str = jQuery(this).parents("form").attr('id');
+		str = str.substring(16);
+		setproducttype(jQuery(this).parents("form"),str);
+	});
 
 });
 
 function sendtocart(form){
 
-	     var datas = jQuery(form).serialize();
-		jQuery.post('index.php?option=com_virtuemart&view=cart&task=addJS&format=raw', datas, 
+	var datas = jQuery(form).serialize();
+	jQuery.post('index.php?option=com_virtuemart&view=cart&task=addJS&format=raw', datas, 
 	
-		function(datas, textStatus) {
-			alert(datas+' '+textStatus);
+	function(datas, textStatus) {
+		alert(datas+' '+textStatus);
 /*			if(datas==1){
 				alert(datas);
 			}else{
 				alert('Product not added to cart, may out of stock ');
 			}*/
-		});
+	});
 
 };
 
 function setproducttype(form,id){
 
 	var datas = jQuery(form).serialize();
-
+	jQuery("#productPrice"+id).fadeTo("slow", 0.33);
 	jQuery.getJSON('index.php?option=com_virtuemart&view=productdetails&task=recalculate&format=json',encodeURIComponent(datas),
 	
 		function(datas, textStatus) {
 			var pid= '';
+			jQuery("#productPrice"+id).fadeTo("slow", 1);
 //	toggle the div Prices
 			for(key in datas) {
 				var value = datas[key];
-				pid= ("#productPrice"+id+" .Price"+key);
+				pid= ("#productPrice"+id+" div .Price"+key);
 				togglePriceVisibility(value,pid);
 			}
 		});
