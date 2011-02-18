f5db0ace7fc2fa8f6afcfb77c615c20eabf2afc9
Electrocity <Electrocity@87cc427b-4a1e-0410-b729-b2260ec0f063>
2011-02-18 12:39:21 +0000

    template file for mod currencies
    git-svn-id: https://dev.virtuemart.net/svn/virtuemart/trunk/virtuemart@2740 87cc427b-4a1e-0410-b729-b2260ec0f063

--- modules/mod_virtuemart_search/tmpl/default.php
+++ modules/mod_virtuemart_search/tmpl/default.php
@@ -1,43 +1,10 @@
 <?php // no direct access
 defined('_JEXEC') or die('Restricted access'); ?>
-<!--BEGIN Search Box -->
-<form action="<?php echo JRoute::_('index.php?option=com_virtuemart&view=category&search=true&category_id='.$category_id ); ?>" method="get">
-<div class="search<?php echo $params->get('moduleclass_sfx'); ?>">
-<?php $output = '<input style="height:16px;vertical-align :middle;" name="keyword" id="mod_virtuemart_search" maxlength="'.$maxlength.'" alt="'.$button_text.'" class="inputbox'.$moduleclass_sfx.'" type="text" size="'.$width.'" value="'.$text.'"  onblur="if(this.value==\'\') this.value=\''.$text.'\';" onfocus="if(this.value==\''.$text.'\') this.value=\'\';" />'; 
- $image = JURI::base().'componet/com_virtuemart/assets/images/vmgeneral/search.png' ;
- 			
-			if ($button) :
-			    if ($imagebutton) :
-			        $button = '<input style="vertical-align :middle;height:16px;border: 1px solid #CCC;" type="image" value="'.$button_text.'" class="button'.$moduleclass_sfx.'" src="'.$image.'" onclick="this.form.keyword.focus();"/>';
-			    else :
-			        $button = '<input type="submit" value="'.$button_text.'" class="button'.$moduleclass_sfx.'" onclick="this.form.keyword.focus();"/>';
-			    endif;
-			endif;
 
-			switch ($button_pos) :
-			    case 'top' :
-				    $button = $button.'<br />';
-				    $output = $button.$output;
-				    break;
-
-			    case 'bottom' :
-				    $button = '<br />'.$button;
-				    $output = $output.$button;
-				    break;
-
-			    case 'right' :
-				    $output = $output.$button;
-				    break;
-
-			    case 'left' :
-			    default :
-				    $output = $button.$output;
-				    break;
-			endswitch;
-
-			echo $output;
-?>
-</div>
+<!-- Currency Selector Module -->
+<?php echo $text_before ?>
+<form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post">
+	<br />
+	<?php echo JHTML::_('select.genericlist', $currencies, 'currency_id', 'class="inputbox"', 'currency_id', 'currency_txt', $currency_id) ; ?>
+    <input class="button" type="submit" name="submit" value="<?php echo JText::_('Change Currency') ?>" />
 </form>
-
-<!-- End Search Box -->
