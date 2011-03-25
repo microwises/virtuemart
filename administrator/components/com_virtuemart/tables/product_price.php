70caf38dd14de755f9ffbdaded4e82d622281e3b
alatak <alatak@87cc427b-4a1e-0410-b729-b2260ec0f063>
2011-03-25 18:55:35 +0000

    Bug #339  : BE view product > cant edit currency nor costprice
    git-svn-id: https://dev.virtuemart.net/svn/virtuemart/trunk/virtuemart@2902 87cc427b-4a1e-0410-b729-b2260ec0f063

--- administrator/components/com_virtuemart/tables/product_price.php
+++ administrator/components/com_virtuemart/tables/product_price.php
@@ -1,21 +1,21 @@
 <?php
-/**
-*
-* Product table
-*
-* @package	VirtueMart
-* @subpackage Product
-* @author RolandD
-* @link http://www.virtuemart.net
-* @copyright Copyright (c) 2004 - 2010 VirtueMart Team. All rights reserved.
-* @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.php
-* VirtueMart is free software. This version may have been modified pursuant
-* to the GNU General Public License, and as distributed it includes or
-* is derivative of works licensed under the GNU General Public License or
-* other free or open source software licenses.
-* @version $Id$
-*/
 
+/**
+ *
+ * Product table
+ *
+ * @package	VirtueMart
+ * @subpackage Product
+ * @author RolandD
+ * @link http://www.virtuemart.net
+ * @copyright Copyright (c) 2004 - 2010 VirtueMart Team. All rights reserved.
+ * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.php
+ * VirtueMart is free software. This version may have been modified pursuant
+ * to the GNU General Public License, and as distributed it includes or
+ * is derivative of works licensed under the GNU General Public License or
+ * other free or open source software licenses.
+ * @version $Id$
+ */
 // Check to ensure this file is included in Joomla!
 defined('_JEXEC') or die('Restricted access');
 
@@ -29,91 +29,93 @@ defined('_JEXEC') or die('Restricted access');
  */
 class TableProduct_price extends JTable {
 
-	/** @var int Primary key */
-	var $product_price_id = 0;
-	/** @var int Product id */
-	var $product_id	= 0;
-	/** @var string Product price */
-	var $product_price = null;
-	
-	var $override  = 0;
-	var $product_override_price = 0;
-	var $product_tax_id = 0;
-	var $product_discount_id = 0;
-	
-	/** @var string Product currency */
-	var $product_currency = null;
+    /** @var int Primary key */
+    var $product_price_id = 0;
+    /** @var int Product id */
+    var $product_id = 0;
+    /** @var string Product price */
+    var $product_price = null;
+    var $override = 0;
+    var $product_override_price = 0;
+    var $product_tax_id = 0;
+    var $product_discount_id = 0;
+    /** @var string Product currency */
+    var $product_currency = null;
+
+    var $product_price_vdate = null;
+    
+    var $product_price_edate = null;
     /** @var string Creation date */
-	var $cdate = null;
+    var $cdate = null;
     /** @var string Modified date */
-	var $mdate = null;
-	/** @var int Shopper group ID */
-	var $shopper_group_id = null;
-	/** @var int Price quantity start */
-	var $price_quantity_start = null;
-	/** @var int Price quantity end */
-	var $price_quantity_end = null;
+    var $mdate = null;
+    /** @var int Shopper group ID */
+    var $shopper_group_id = null;
+    /** @var int Price quantity start */
+    var $price_quantity_start = null;
+    /** @var int Price quantity end */
+    var $price_quantity_end = null;
 
-	/**
-	 * @author RolandD
-	 * @param $db A database connector object
-	 */
-	function __construct(&$db) {
-		parent::__construct('#__vm_product_price', 'product_price_id', $db);
-	}
+    /**
+     * @author RolandD
+     * @param $db A database connector object
+     */
+    function __construct(&$db) {
+        parent::__construct('#__vm_product_price', 'product_price_id', $db);
+    }
 
-	/**
-	 * @author Max Milbers
-	 * @param 
-	 */	
-	function check (){
-		
-		if (!$this->product_id) {
-			$this->setError(JText::_('VM_IMPOSSIBLE_TO_SAVE_PRODUCT_PRICES_WITHOUT_PRODUCT_ID'));
-			return false;
-		}
-		
+    /**
+     * @author Max Milbers
+     * @param
+     */
+    function check() {
+
+        if (!$this->product_id) {
+            $this->setError(JText::_('VM_IMPOSSIBLE_TO_SAVE_PRODUCT_PRICES_WITHOUT_PRODUCT_ID'));
+            return false;
+        }
+
 //		if (!$this->product_price) {
 //			$this->setError(JText::_('VM_IMPOSSIBLE_TO_SAVE_PRODUCT_PRICES_WITHOUT_PRODUCT_PRICE'));
 //			return false;
 //		}
-
 //		if (!$this->product_currency) {
 //			$this->setError(JText::_('VM_IMPOSSIBLE_TO_SAVE_PRODUCT_PRICES_WITHOUT_PRODUCT_CURRENCY'));
 //			return false;
 //		}
-		return true;
-	}
-	
-	 /**
-	 * Records in this table do not need to exist, so we might need to create a record even
-	 * if the primary key is set. Therefore we need to overload the store() function.
-	 * 
-	 * @author Oscar van Eijk
-	 * @author Max Milbers
-	 * @see libraries/joomla/database/JTable#store($updateNulls)
-	 */
-	public function store()
-	{
-		$_qry = 'SELECT product_id '
-				. 'FROM #__vm_product_price '
-				. 'WHERE product_price_id = ' . $this->product_price_id
-		;
-		$this->_db->setQuery($_qry);
-		$_count = $this->_db->loadResultArray();
+        return true;
+    }
 
-		if (count($_count) > 0) {
-			$returnCode = $this->_db->updateObject( $this->_tbl, $this, $this->_tbl_key, false );
-		} else {
-			$returnCode = $this->_db->insertObject( $this->_tbl, $this, $this->_tbl_key);
-		}
+    /**
+     * Records in this table do not need to exist, so we might need to create a record even
+     * if the primary key is set. Therefore we need to overload the store() function.
+     *
+     * @author Oscar van Eijk
+     * @author Max Milbers
+     * @see libraries/joomla/database/JTable#store($updateNulls)
+     */
+    public function store() {
+        $_qry = 'SELECT product_id '
+                . 'FROM #__vm_product_price '
+                . 'WHERE product_price_id = ' . $this->product_price_id
+        ;
+        $this->_db->setQuery($_qry);
+        $_count = $this->_db->loadResultArray();
 
-		if (!$returnCode){
-			$this->setError(get_class( $this ).'::store failed - '.$this->_db->getErrorMsg());
-			return false;
-		}
-		else return true;
-	}
+        if (count($_count) > 0) {
+            $returnCode = $this->_db->updateObject($this->_tbl, $this, $this->_tbl_key, false);
+        } else {
+            $returnCode = $this->_db->insertObject($this->_tbl, $this, $this->_tbl_key);
+        }
+
+        if (!$returnCode) {
+            $this->setError(get_class($this) . '::store failed - ' . $this->_db->getErrorMsg());
+            return false;
+        }
+        else
+            return true;
+    }
 
 }
+
 // pure php no closing tag
