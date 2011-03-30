73b667776e549b9a8d5a93c983d7c5e6090d9321
oscar <oscar@87cc427b-4a1e-0410-b729-b2260ec0f063>
2011-03-30 14:14:10 +0000

    Fix for bug #245; implemented the checkout config form with the check_stock setting as currently only option
    git-svn-id: https://dev.virtuemart.net/svn/virtuemart/trunk/virtuemart@2918 87cc427b-4a1e-0410-b729-b2260ec0f063

--- administrator/components/com_virtuemart/views/config/tmpl/default_checkout.php
+++ administrator/components/com_virtuemart/views/config/tmpl/default_checkout.php
@@ -1,22 +1,34 @@
 <?php
 /**
-*
-* Description
-*
-* @package	VirtueMart
-* @subpackage Config
-* @author RickG
-* @link http://www.virtuemart.net
-* @copyright Copyright (c) 2004 - 2010 VirtueMart Team. All rights reserved.
-* @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.php
-* VirtueMart is free software. This version may have been modified pursuant
-* to the GNU General Public License, and as distributed it includes or
-* is derivative of works licensed under the GNU General Public License or
-* other free or open source software licenses.
-* @version $Id$
-*/
-
-// Check to ensure this file is included in Joomla!
-defined('_JEXEC') or die('Restricted access');
-
-echo 'At the moment without use';
+ * Admin form for the checkout configuration settings
+ *
+ * @package	VirtueMart
+ * @subpackage Config
+ * @author Oscar van Eijk
+ * @link http://www.virtuemart.net
+ * @copyright Copyright (c) 2004 - 2011 VirtueMart Team. All rights reserved.
+ * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.php
+ * VirtueMart is free software. This version may have been modified pursuant
+ * to the GNU General Public License, and as distributed it includes or
+ * is derivative of works licensed under the GNU General Public License or
+ * other free or open source software licenses.
+ * @version $Id$
+ */
+defined('_JEXEC') or die('Restricted access'); 
+?> 
+<br />
+<fieldset class="adminform">
+	<legend><?php echo JText::_('VM_ADMIN_CFG_CHECKOUT_SETTINGS') ?></legend>
+	<table class="admintable">
+		<tr>
+			<td class="key">
+				<span class="editlinktip hasTip" title="<?php echo JText::_('VM_ADMIN_CFG_CHECK_STOCK_EXPLAIN'); ?>">
+					<label for="coupons_enable"><?php echo JText::_('VM_ADMIN_CFG_CHECK_STOCK') ?></label>
+				</span>
+			</td>
+			<td>
+				<?php echo VmHTML::checkbox('check_stock', $this->config->get('check_stock')); ?>
+			</td>
+		</tr>
+	</table>
+</fieldset>
