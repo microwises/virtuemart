0408c2131ce8320dec09517b8c465012b57cd91b
wicksj <wicksj@87cc427b-4a1e-0410-b729-b2260ec0f063>
2011-04-20 16:29:33 +0000

    Added switch for tasks and helper functions
    git-svn-id: https://dev.virtuemart.net/svn/virtuemart/trunk/virtuemart@3064 87cc427b-4a1e-0410-b729-b2260ec0f063

--- administrator/components/com_virtuemart/views/report/view.html.php
+++ administrator/components/com_virtuemart/views/report/view.html.php
@@ -36,42 +36,77 @@ class VirtuemartViewReport extends JView {
 		$lists = array();
 		$mainframe = JFactory::getApplication();
 		$option = JRequest::getVar('option');
+		$config   =& JFactory::getConfig();
 		$curTask = JRequest::getVar('task');
 		$layoutName = JRequest::getVar('layout','default');
+
+		$from_period  = JRequest::getVar('from_period', '?');
+		$until_period = JRequest::getVar('until_period', '?');
 
 		JToolbarHelper::title( JText::_('COM_VIRTUEMART_REPORT_MOD'), 'vm_report_48');
 
 		// Load the helper(s)
 		$this->loadHelper('adminMenu');
 		$this->loadHelper('currencydisplay');
+		$this->loadHelper('reportFunctions');
 
 		$model = $this->getModel();
 
-		$pagination = $model->getPagination();
-		$lists['filter_order'] = $mainframe->getUserStateFromRequest($option.'filter_order', 'filter_order', '', 'cmd');
-	    $lists['filter_order_Dir'] = $mainframe->getUserStateFromRequest($option.'filter_order_Dir', 'filter_order_Dir', '', 'word');
+		switch($curTask){
+			default:{
+				$pagination = $model->getPagination();
+				$lists['filter_order'] = $mainframe->getUserStateFromRequest($option.'filter_order', 'filter_order', '', 'cmd');
+				$lists['filter_order_Dir'] = $mainframe->getUserStateFromRequest($option.'filter_order_Dir', 'filter_order_Dir', '', 'word');
 
-		$this->assignRef('pagination', $pagination);
+				$date_presets = ReportFunctions::getDatePresets();
+				$lists['select_date'] = ReportFunctions::renderDateSelectList($date_presets, $from_period, $until_period);
+				// set period
+				$tzoffset     = $config->getValue('config.offset');
+				$from         = JFactory::getDate($from_period, $tzoffset);
+				$until        = JFactory::getDate($until_period, $tzoffset);
+				
+		
+				// check period - set to defaults if no value is set or dates cannot be parsed
+				if ($from->_date === false || $until->_date === false) {
+					if ($from_period != '?' && $until_period != '?') {
+						JError::raiseNotice(500, JText::_('Please enter a valid date format (YYYY-MM-DD)'));
+					}
+					$from_period  = $date_presets['last30']['from'];
+					$until_period = $date_presets['last30']['until'];
+					$from         = JFactory::getDate($from_period, $tzoffset);
+					$until        = JFactory::getDate($until_period, $tzoffset);
+				} else {
+					if ($from->toUnix() > $until->toUnix()){
+						list($from_period, $until_period) = array($until_period, $from_period);
+						list($from, $until) = array($until, $from);
+					}
+				}		
 
-		$myCurrencyDisplay = CurrencyDisplay::getCurrencyDisplay();
+				$this->assignRef('pagination', $pagination);
 
-		$revenueBasic = $model->getRevenue();
-        if(is_array($revenueBasic)){
-		    foreach($revenueBasic as $i => $j){
-			    $j->revenue = $myCurrencyDisplay->getValue($j->revenue);
-		    }
-            unset($i);
-        }
+				$myCurrencyDisplay = CurrencyDisplay::getCurrencyDisplay();
 
-		$this->assignRef('revenueBasic', $revenueBasic);
+				$revenueBasic = $model->getRevenue();
+				if(is_array($revenueBasic)){
+					foreach($revenueBasic as $i => $j){
+						$j->revenue = $myCurrencyDisplay->getValue($j->revenue);
+					}
+					unset($i);
+				}
 
-		$itemsSold = $model->getItemsSold();
-		$this->assignRef('itemsSold', $itemsSold);
+				$this->assignRef('report', $revenueBasic);
 
-		$productList = $model->getProductList();
-		$this->assignRef('productList', $productList);
+				$itemsSold = $model->getItemsSold();
+				$this->assignRef('itemsSold', $itemsSold);
 
-		$this->assignRef('lists', $lists);
+				$productList = $model->getProductList();
+				$this->assignRef('productList', $productList);
+
+				$this->assignRef('lists', $lists);
+				$this->assignRef('from_period', $from_period);
+				$this->assignRef('until_period', $until_period);
+			}
+		}
 
 		parent::display($tpl);
 	}
