38e6d5901745d148e41739b17686610373a44a51
impleri <impleri@87cc427b-4a1e-0410-b729-b2260ec0f063>
2011-05-04 10:59:36 +0000

    A few minor bugs in the template for update order status email (missing order no.)
    git-svn-id: https://dev.virtuemart.net/svn/virtuemart/trunk/virtuemart@3161 87cc427b-4a1e-0410-b729-b2260ec0f063

--- administrator/components/com_virtuemart/views/orders/tmpl/mail_html_updorder.php
+++ administrator/components/com_virtuemart/views/orders/tmpl/mail_html_updorder.php
@@ -1,9 +1,10 @@
-<?php echo JText::_('COM_VIRTUEMART_HI') .' '. $this->user->full_name . "\n";
-echo JText::_('COM_VIRTUEMART_ORDER_STATUS_CHANGE_SEND_MSG_1') . "\n"  . "\n";
+<?php
+echo JText::_('COM_VIRTUEMART_HI') .' '. $this->user->full_name . "\n";
+echo JText::sprintf('COM_VIRTUEMART_ORDER_STATUS_CHANGE_SEND_MSG_1', $this->order->order_id) . "\n"  . "\n";
 
 if (array_key_exists($this->order->order_id, $this->includeComments) && !empty($this->includeComments)) {
 	echo JText::_('COM_VIRTUEMART_ORDER_HISTORY_COMMENT_EMAIL').":"  . "\n";
-	echo $this->includeComments  . "\n";
+	echo implode("\n", $this->includeComments)  . "\n";
 	echo ' ____________________________________________________________'  . "\n";
 }
 
