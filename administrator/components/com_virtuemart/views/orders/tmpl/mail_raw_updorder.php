51e3d757be536fa33347f4c7d0eea4c0a98442d0
Christopher Roussel <christopher@impleri.net>
2011-05-04 11:57:57 +0100

    A few minor bugs in the template for update order status email (missing order no.)

--- administrator/components/com_virtuemart/views/orders/tmpl/mail_raw_updorder.php
+++ administrator/components/com_virtuemart/views/orders/tmpl/mail_raw_updorder.php
@@ -1,19 +1,20 @@
-<?php echo JText::_('COM_VIRTUEMART_HI',false) .' '. $this->user->full_name . "\n";
-echo JText::_('COM_VIRTUEMART_ORDER_STATUS_CHANGE_SEND_MSG_1',false) . "\n"  . "\n";
+<?php
+echo JText::_('COM_VIRTUEMART_HI') .' '. $this->user->full_name . "\n";
+echo JText::sprintf('COM_VIRTUEMART_ORDER_STATUS_CHANGE_SEND_MSG_1', $this->order->order_id) . "\n"  . "\n";
 
-if (array_key_exists($this->order->order_id, $this->includeComments) && !empty($this->_comments)) {
-	echo JText::_('COM_VIRTUEMART_ORDER_HISTORY_COMMENT_EMAIL',false).":"  . "\n";
-	echo $this->_comments  . "\n";
+if (array_key_exists($this->order->order_id, $this->includeComments) && !empty($this->includeComments)) {
+	echo JText::_('COM_VIRTUEMART_ORDER_HISTORY_COMMENT_EMAIL').":"  . "\n";
+	echo implode("\n", $this->includeComments)  . "\n";
 	echo ' ____________________________________________________________'  . "\n";
 }
 
-echo JText::_('COM_VIRTUEMART_ORDER_STATUS_CHANGE_SEND_MSG_2',false)  . "\n";
+echo JText::_('COM_VIRTUEMART_ORDER_STATUS_CHANGE_SEND_MSG_2')  . "\n";
 echo '____________________________________________________________'  . "\n";
 echo $this->user->order_status_name  . "\n"  . "\n";
 
 if (VmConfig::get('vm_registration_type') != 'NO_REGISTRATION' ) {
 	echo '____________________________________________________________'  . "\n";
-	echo JText::_('COM_VIRTUEMART_ORDER_STATUS_CHANGE_SEND_MSG_3',false)  . "\n";
+	echo JText::_('COM_VIRTUEMART_ORDER_STATUS_CHANGE_SEND_MSG_3')  . "\n";
 	echo $this->url  . "\n"  . "\n";
 }
 echo '____________________________________________________________'  . "\n";
