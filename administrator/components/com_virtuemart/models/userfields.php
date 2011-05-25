a59b56b7f799c0b21048136f37e260553ffd30f8
Christopher Roussel <christopher@impleri.net>
2011-05-25 20:30:22 +0100

    Keep delimiters from updating tables in userfields

--- administrator/components/com_virtuemart/models/userfields.php
+++ administrator/components/com_virtuemart/models/userfields.php
@@ -244,7 +244,7 @@ class VirtueMartModelUserfields extends VmModel {
 		$_fieldType = $field->formatFieldType($data);
 
 		$coreFields= array( 'username', 'email', 'password', 'password2' );
-		if(!in_array($data['name'],$coreFields)){
+		if(!in_array($data['name'],$coreFields) && $_fieldType != 'delimiter'){
 
 			// Alter the user_info table
 			if (!$userinfo->_modifyColumn ($_action, $data['name'], $_fieldType)) {
