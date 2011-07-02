3afe3aaa1065057eb280340a2610704a4dd13a8b
electrocity <electrocity@87cc427b-4a1e-0410-b729-b2260ec0f063>
2011-07-02 16:42:39 +0000

    Fix blank page for some server
    git-svn-id: https://dev.virtuemart.net/svn/virtuemart/trunk/virtuemart@3596 87cc427b-4a1e-0410-b729-b2260ec0f063

--- administrator/components/com_virtuemart/views/category/tmpl/default.php
+++ administrator/components/com_virtuemart/views/category/tmpl/default.php
@@ -109,13 +109,17 @@ if( $this->pagination->limit < $nrows ){
 				<td align="center">
 					<?php echo $published;?>
 				</td>
-				<!-- Commented out for future use
+<?php
+		/*
+		 * html comment do a bug in some server
+		 * Used in the future Notice by Patrick Kohl
 				<td align="center">
 					<a href="#" onclick="return listItemTask('cb<?php echo $i;?>', 'toggleShared')" title="<?php echo ( $row->category_shared == 'Y' ) ?JText::_('COM_VIRTUEMART_YES') : JText::_('COM_VIRTUEMART_NO');?>">
 						<img src="images/<?php echo ( $row->category_shared) ? 'tick.png' : 'publish_x.png';?>" width="16" height="16" border="0" alt="<?php echo ( $row->category_shared == 'Y' ) ? JText::_('COM_VIRTUEMART_YES') : JText::_('COM_VIRTUEMART_NO');?>" />
 					</a>
 				</td>
-				-->
+				*/
+?>
 				<td align="center" class="order">
 					<span><?php echo $this->pagination->orderUpIcon( $i, ($row->category_parent_id == 0 || $row->category_parent_id == @$this->categories[$this->rowList[$i - 1]]->category_parent_id), 'orderUp', 'Move Up'); ?></span>
 					<span><?php echo $this->pagination->orderDownIcon( $i, $nrows, ($row->category_parent_id == 0 || $row->category_parent_id == @$this->categories[$this->rowList[$i + 1]]->category_parent_id), 'orderDown', 'Move Down'); ?></span>
