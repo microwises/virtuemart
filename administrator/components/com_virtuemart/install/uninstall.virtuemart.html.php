34b1c8c67c09dcd3502d0d9c38c2faba1088716f
alatak <alatak@87cc427b-4a1e-0410-b729-b2260ec0f063>
2011-07-14 07:44:50 +0000

    typo
    git-svn-id: https://dev.virtuemart.net/svn/virtuemart/trunk/virtuemart@3708 87cc427b-4a1e-0410-b729-b2260ec0f063

--- administrator/components/com_virtuemart/install/uninstall.virtuemart.html.php
+++ administrator/components/com_virtuemart/install/uninstall.virtuemart.html.php
@@ -10,14 +10,14 @@
 	<table width="100%" border="0">
 	<tr>
 		<td>
-			<h1><?php echo JText::_('COM_VIRTUEMART_UNISTALL_THANKYOU') ; ?></h1>
+			<h1><?php echo JText::_('COM_VIRTUEMART_UNINSTALL_THANKYOU') ; ?></h1>
 		</td>
 	</tr>
 	<tr>
 		<td valign="top" align="center">
-			<h2><?php echo JText::_('COM_VIRTUEMART_UNISTALL_NOTE') ; ?></h2>
+			<h2><?php echo JText::_('COM_VIRTUEMART_UNINSTALL_NOTE') ; ?></h2>
 			<p>
-				<big><?php echo JText::_('COM_VIRTUEMART_UNISTALL_NOTE_DESC') ; ?></big>
+				<big><?php echo JText::_('COM_VIRTUEMART_UNINSTALL_NOTE_DESC') ; ?></big>
 			</p>
 		</td>
 	</tr>	
