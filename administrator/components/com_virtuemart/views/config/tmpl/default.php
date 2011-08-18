18d8b0afb7e0ee006e2e8bc273b03473cbff59cf
mike75 <mike75@87cc427b-4a1e-0410-b729-b2260ec0f063>
2011-08-18 18:12:44 +0000

    remove soap tab
    git-svn-id: https://dev.virtuemart.net/svn/virtuemart/trunk/virtuemart@3906 87cc427b-4a1e-0410-b729-b2260ec0f063

--- administrator/components/com_virtuemart/views/config/tmpl/default.php
+++ administrator/components/com_virtuemart/views/config/tmpl/default.php
@@ -28,8 +28,7 @@ AdminUIHelper::buildTabs ( array (	'shop' 			=> 	'COM_VIRTUEMART_ADMIN_CFG_SHOPT
 									'product_order'	=> 	'COM_VIRTUEMART_ADMIN_CFG_PRODUCTORDERTAB',
 									'pricing' 		=> 	'COM_VIRTUEMART_ADMIN_CFG_PRICINGTAB',
 									'checkout' 		=> 	'COM_VIRTUEMART_ADMIN_CFG_CHECKOUTTAB',
-									'sef' 			=> 	'COM_VIRTUEMART_ADMIN_CFG_SEF',
-									'soap' 			=> 	'COM_VIRTUEMART_ADMIN_CFG_SOAP' ) );
+									'sef' 			=> 	'COM_VIRTUEMART_ADMIN_CFG_SEF' );
 
 ?>
 
