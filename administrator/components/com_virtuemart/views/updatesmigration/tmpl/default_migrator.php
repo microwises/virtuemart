d61a21dc483e99c3244f9f45775944724aba392f
Milbo <Milbo@87cc427b-4a1e-0410-b729-b2260ec0f063>
2011-09-11 20:15:45 +0000

    forgot a file
    git-svn-id: https://dev.virtuemart.net/svn/virtuemart/trunk/virtuemart@4077 87cc427b-4a1e-0410-b729-b2260ec0f063

---
+++ administrator/components/com_virtuemart/views/updatesmigration/tmpl/default_migrator.php
@@ -0,0 +1,85 @@
+<?php
+/**
+*
+* Description
+*
+* @package	VirtueMart
+* @subpackage UpdatesMigration
+* @author Max Milbers
+* @link http://www.virtuemart.net
+* @copyright Copyright (c) 2004 - 2011 VirtueMart Team. All rights reserved.
+* @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.php
+* VirtueMart is free software. This version may have been modified pursuant
+* to the GNU General Public License, and as distributed it includes or
+* is derivative of works licensed under the GNU General Public License or
+* other free or open source software licenses.
+* @version $Id: default_tools.php 4007 2011-08-31 07:31:35Z alatak $
+*/
+
+?>
+<form action="index.php" method="post" name="adminForm" enctype="multipart/form-data" >
+<input type="hidden" name="task" value="" />
+
+<table>
+<tr>
+	<td align="left" colspan="5" >
+		<h3> <?php echo JText::_('COM_VIRTUEMART_UPDATE_MIGRATION_TITLE'); ?> </h3>
+	</td>
+</tr>
+<tr>
+    <td align="center">
+		<button class="default" type="submit" ><?php echo JText::_('COM_VIRTUEMART_MIGRATE'); ?></button>
+    </td>
+<tr>
+
+<tr>
+	<td>
+		<?php echo JText::_('COM_VIRTUEMART_UPDATE_MIGRATION_STRING'); ?>
+	</td>
+	<td>
+   <?php
+		$options = array(
+			'migrateGeneralFromVmOne'	=>	JText::_('COM_VIRTUEMART_UPDATE_GENERAL'),
+			'migrateUsersFromVmOne'	=>	JText::_('COM_VIRTUEMART_UPDATE_USERS'),
+			'migrateProductsFromVmOne'	=> JText::_('COM_VIRTUEMART_UPDATE_PRODUCTS'),
+			'migrateOrdersFromVmOne'	=> JText::_('COM_VIRTUEMART_UPDATE_ORDERS'),
+			'migrateAllInOne'	=> JText::_('COM_VIRTUEMART_UPDATE_ALL')
+		);
+		echo VmHTML::radioList('task', 'all', $options);
+	?>
+	</td>
+</tr>
+
+<tr>
+	<td>
+		<?php echo JText::_('COM_VIRTUEMART_MIGRATION_DCAT_BROWSE'); ?>
+	</td>
+	<td>
+		<input class="inputbox" type="text" name="default_category_browse" size="15" value="" />
+	</td>
+</tr>
+
+<tr>
+	<td>
+		<?php echo JText::_('COM_VIRTUEMART_MIGRATION_DCAT_FLY'); ?>
+	</td>
+	<td>
+		<input class="inputbox" type="text" name="default_category_fly" size="15" value="" />
+	</td>
+</tr>
+
+<tr>
+	<td>
+		<?php echo JText::_('COM_VIRTUEMART_MIGRATION_DPROD_FLY'); ?>
+	</td>
+	<td>
+		<input class="inputbox" type="text" name="default_product_fly" size="15" value="" />
+	</td>
+</tr>
+
+</table>
+<!-- Hidden Fields -->
+<input type="hidden" name="option" value="com_virtuemart" />
+<input type="hidden" name="view" value="updatesMigration" />
+<?php echo JHTML::_( 'form.token' ); ?>
+</form>
