7d57a5edb46b61b66ef4c9f6ffd6c8f708924d7e
Milbo <Milbo@87cc427b-4a1e-0410-b729-b2260ec0f063>
2011-10-03 20:53:37 +0000

    the strange array message gone?
    git-svn-id: https://dev.virtuemart.net/svn/virtuemart/trunk/virtuemart@4233 87cc427b-4a1e-0410-b729-b2260ec0f063

--- components/com_virtuemart/controllers/user.php
+++ components/com_virtuemart/controllers/user.php
@@ -65,45 +65,11 @@ class VirtueMartControllerUser extends JController
 		jimport( 'joomla.utilities.arrayhelper' );
 		JArrayHelper::toInteger($cid);
 
-
-
-//		$ftask ='saveuser';
-//		$view->assignRef('fTask', $ftask);
-
 		/* Display it all */
 		$view->display();
 
 	}
 
-	/**
-	 * This is the save function for the normal user edit.php layout.
-	 * We use here directly the userModel store function, because this view is for registering also
-	 * it redirects to the standard user view.
-	 *
-	 * @author Max Milbers
-	 */
-	function saveUser(){
-		$this->addModelPath( JPATH_VM_ADMINISTRATOR.DS.'models' );
-		$userModel = $this->getModel('user');
-
-		$data = JRequest::get('post');
-
-		// Store multiple selectlist entries as a ; separated string
-		if (key_exists('vendor_accepted_currencies', $data) && is_array($data['vendor_accepted_currencies'])) {
-		    $data['vendor_accepted_currencies'] = implode(',', $data['vendor_accepted_currencies']);
-		}
-
-		$data['vendor_store_name'] = JRequest::getVar('vendor_store_name','','post','STRING',JREQUEST_ALLOWHTML);
-		$data['vendor_store_desc'] = JRequest::getVar('vendor_store_desc','','post','STRING',JREQUEST_ALLOWHTML);
-		$data['vendor_terms_of_service'] = JRequest::getVar('vendor_terms_of_service','','post','STRING',JREQUEST_ALLOWHTML);
-
-
-		$ret = $userModel->store($data);
-		$msg = (is_array($ret)) ? $ret['message'] : $ret;
-
-		$this->saveToCart();
-		$this->setRedirect( JRoute::_('index.php?option=com_virtuemart&view=user'), $msg );
-	}
 
 	function editAddressSt(){
 
@@ -140,8 +106,6 @@ class VirtueMartControllerUser extends JController
 
 		$ftask ='savecartuser';
 		$view->assignRef('fTask', $ftask);
-//		$ctask ='cancelcartuser';		//Looks like we dont need it, but atm not sure
-//		$view->assignRef('cTask', $ctask);
 
 		/* Display it all */
 		$view->display();
@@ -166,21 +130,9 @@ class VirtueMartControllerUser extends JController
 
 		$ftask ='savecheckoutuser';
 		$view->assignRef('fTask', $ftask);
-//		$ctask ='cancelcheckoutuser';
-//		$view->assignRef('cTask', $ctask);
 
 		/* Display it all */
 		$view->display();
-	}
-
-	/**
-	 * Editing a user address was cancelled when called from the cart; return to the cart
-	 *
-	 * @author Oscar van Eijk
-	 */
-	function cancelCartUser(){
-//		$this->setRedirect( 'index.php?option=com_virtuemart&view=cart&task=checkout', $msg );
-		$this->setRedirect( JRoute::_('index.php?option=com_virtuemart&view=cart'), $msg );
 	}
 
 	/**
@@ -192,7 +144,6 @@ class VirtueMartControllerUser extends JController
 	function saveCheckoutUser(){
 
 		$msg = $this->saveData(true);
-		$this->saveToCart();
 
 		//We may add here the option for silent registration.
 		$this->setRedirect( JRoute::_('index.php?option=com_virtuemart&view=cart&task=checkout',$this->useXHTML,$this->useSSL), $msg );
@@ -200,7 +151,6 @@ class VirtueMartControllerUser extends JController
 
 	function registerCheckoutUser(){
 		$msg = $this->saveData(true,true);
-		$this->saveToCart();
 		$this->setRedirect(JRoute::_( 'index.php?option=com_virtuemart&view=cart&task=checkout',$this->useXHTML,$this->useSSL ),$msg);
 	}
 
@@ -213,54 +163,58 @@ class VirtueMartControllerUser extends JController
 	function saveCartUser(){
 
 		$msg = $this->saveData(true);
-		$this->saveToCart();
 		$this->setRedirect(JRoute::_( 'index.php?option=com_virtuemart&view=cart' ),$msg);
 	}
 
 	function registerCartuser(){
 
 		$msg = $this->saveData(true,true);
-		$this->saveToCart();
 		$this->setRedirect(JRoute::_( 'index.php?option=com_virtuemart&view=cart' ),$msg);
 	}
 
 
 	/**
-	 * Editing a user address was cancelled during chaeckout; return to the cart
-	 *
-	 * @author Oscar van Eijk
-	 */
-	function cancelCheckoutUser(){
-		$this->setRedirect( JRoute::_('index.php?option=com_virtuemart&view=cart&task=checkout',$this->useXHTML,$this->useSSL), $msg );
-	}
-
-	/**
-	 * This function just gets the post data and put the data if there is any to the cart
-	 *
-	 * @author Max Milbers
-	 */
-	private function saveToCart(){
+	* This is the save function for the normal user edit.php layout.
+	* We use here directly the userModel store function, because this view is for registering also
+	* it redirects to the standard user view.
+	*
+	* @author Max Milbers
+	*/
+	function saveUser(){
+		$this->addModelPath( JPATH_VM_ADMINISTRATOR.DS.'models' );
+		$userModel = $this->getModel('user');
 
 		$data = JRequest::get('post');
 
-		if(!class_exists('VirtueMartCart')) require(JPATH_VM_SITE.DS.'helpers'.DS.'cart.php');
-		$cart = VirtueMartCart::getCart();
-		$cart->saveAddressInCart($data, $data['address_type']);
+		// Store multiple selectlist entries as a ; separated string
+		if (key_exists('vendor_accepted_currencies', $data) && is_array($data['vendor_accepted_currencies'])) {
+			$data['vendor_accepted_currencies'] = implode(',', $data['vendor_accepted_currencies']);
+		}
 
+		$data['vendor_store_name'] = JRequest::getVar('vendor_store_name','','post','STRING',JREQUEST_ALLOWHTML);
+		$data['vendor_store_desc'] = JRequest::getVar('vendor_store_desc','','post','STRING',JREQUEST_ALLOWHTML);
+		$data['vendor_terms_of_service'] = JRequest::getVar('vendor_terms_of_service','','post','STRING',JREQUEST_ALLOWHTML);
+
+		$ret = $userModel->store($data);
+		$msg = (is_array($ret)) ? $ret['message'] : $ret;
+
+		$this->saveToCart($data);
+		$this->setRedirect( JRoute::_('index.php?option=com_virtuemart&view=user'), $msg );
 	}
 
 	/**
-	 * Save the user info. The saveData function dont use the userModel store function for anonymous shoppers, because it would register them.
-	 * We make this function private, so we can do the tests in the tasks.
-	 *
-	 * @author Max Milbers
-	 *
-	 * @param boolean Defaults to false, the param is for the userModel->store function, which needs it to determin how to handle the data.
-	 * @return String it gives back the messages.
-	 */
+	* Save the user info. The saveData function dont use the userModel store function for anonymous shoppers, because it would register them.
+	* We make this function private, so we can do the tests in the tasks.
+	*
+	* @author Max Milbers
+	*
+	* @param boolean Defaults to false, the param is for the userModel->store function, which needs it to determin how to handle the data.
+	* @return String it gives back the messages.
+	*/
 	private function saveData($cart=false,$register=false) {
 
 		$currentUser = JFactory::getUser();
+		$msg = '';
 		if($currentUser->id!=0 || $register){
 			$this->addModelPath( JPATH_VM_ADMINISTRATOR.DS.'models' );
 			$userModel = $this->getModel('user');
@@ -269,140 +223,51 @@ class VirtueMartControllerUser extends JController
 
 			// Store multiple selectlist entries as a ; separated string
 			if (key_exists('vendor_accepted_currencies', $data) && is_array($data['vendor_accepted_currencies'])) {
-			    $data['vendor_accepted_currencies'] = implode(',', $data['vendor_accepted_currencies']);
+				$data['vendor_accepted_currencies'] = implode(',', $data['vendor_accepted_currencies']);
 			}
 
 			$data['vendor_store_name'] = JRequest::getVar('vendor_store_name','','post','STRING',JREQUEST_ALLOWHTML);
 			$data['vendor_store_desc'] = JRequest::getVar('vendor_store_desc','','post','STRING',JREQUEST_ALLOWHTML);
 			$data['vendor_terms_of_service'] = JRequest::getVar('vendor_terms_of_service','','post','STRING',JREQUEST_ALLOWHTML);
 
-			$msg = $userModel->store($data);
+			$ret = $userModel->store($data);
+			$msg = (is_array($ret)) ? $ret['message'] : $ret;
 		}
-
+		$this->saveToCart($data);
 		return $msg;
+	}
+
+	/**
+	* This function just gets the post data and put the data if there is any to the cart
+	*
+	* @author Max Milbers
+	*/
+	private function saveToCart($data){
+
+		if(!class_exists('VirtueMartCart')) require(JPATH_VM_SITE.DS.'helpers'.DS.'cart.php');
+		$cart = VirtueMartCart::getCart();
+		$cart->saveAddressInCart($data, $data['address_type']);
 
 	}
 
+	/**
+	* Editing a user address was cancelled when called from the cart; return to the cart
+	*
+	* @author Oscar van Eijk
+	*/
+	function cancelCartUser(){
 
-//	public function renderRegisterMailToUser(){
-//
-//		/* Create the view */
-//		$view = $this->getView('user', 'html');
-//
-//		if(!class_exists('VirtueMartCart')) require(JPATH_VM_SITE.DS.'helpers'.DS.'cart.php');
-////		$view->setModel(VirtueMartCart::getCart());		//I must admit that looks a bit strange, because it is a not a model, but an object
-////		$view->assignRef('cart', VirtueMartCart::getCart());
-//
-//		$this->addModelPath( JPATH_VM_ADMINISTRATOR.DS.'models' );
-//		$user = $this->getModel( 'user', 'VirtuemartModel' );
-//		$view->setModel( $user );
-//		$view->setModel( $this->getModel( 'userfields', 'VirtuemartModel' ) );
-//		$view->setModel( $this->getModel( 'orders', 'VirtuemartModel' ) );
-//		$vendor = $this->getModel( 'vendor', 'VirtuemartModel' );
-//		$view->setModel( $vendor );
-//
-//		$view->setLayout('mailregisteruser');
-//
-//		$view->display();
-//
-//	}
-//
-//	public function renderRegisterMailToVendor(){
-//
-//		/* Create the view */
-//		$view = $this->getView('user', 'html');
-//
-//		if(!class_exists('VirtueMartCart')) require(JPATH_VM_SITE.DS.'helpers'.DS.'cart.php');
-//
-//		$this->addModelPath( JPATH_VM_ADMINISTRATOR.DS.'models' );
-//		$user = $this->getModel( 'user', 'VirtuemartModel' );
-//		$view->setModel( $user  );
-//		$view->setModel( $this->getModel( 'userfields', 'VirtuemartModel' ) );
-//		$view->setModel( $this->getModel( 'orders', 'VirtuemartModel' ) );
-//		$vendor = $this->getModel( 'vendor', 'VirtuemartModel' );
-//		$view->setModel( $vendor );
-//
-//		$view->setLayout('mailregistervendor');
-//
-//		$view->display();
-//
-//	}
+		$this->setRedirect( JRoute::_('index.php?option=com_virtuemart&view=cart'), $msg );
+	}
 
 	/**
-	 * Prepares the email body for shopper and vendor, renders them and sends directly the emails
-	 *
-	 * This function is not fully workable. The information useremail and userpassword must be given better.
-	 *
-	 * @author Max Milbers
-	 *
-	 * @param $user JUser
-	 * @param boolean When one email does not work, it gives a false back
-	 *
-	 */
-//	private function doRegisterEmail($user, $password){
-//
-//		if(empty($user)){
-//			echo 'Internal error doRegisterEmail user object empty';
-//			return false;
-//		}
-//		if(empty($password)){
-//			echo 'Internal error doRegisterEmail email empty';
-//			return false;
-//		} else {
-//			$password = preg_replace('/[\x00-\x1F\x7F]/', '', $password); //Disallow control chars in the email
-//		}
-//
-//		/* Create the view */
-//		$view = $this->getView('user', 'html');
-//
-//		if(!class_exists('VirtueMartCart')) require(JPATH_VM_SITE.DS.'helpers'.DS.'cart.php');
-//		$view->setModel(VirtueMartCart::getCart(), true);		//I must admit that looks a bit strange, because it is a not a model, but an object
-//
-//		$this->addModelPath( JPATH_VM_ADMINISTRATOR.DS.'models' );
-//		$user = $this->getModel( 'user', 'VirtuemartModel' );
-//		$view->setModel( $user , false );
-//		$view->setModel( $this->getModel( 'userfields', 'VirtuemartModel' ), true );
-//		$view->setModel( $this->getModel( 'orders', 'VirtuemartModel' ), true );  //TODO we need the order_number in the mail
-//		$vendor = $this->getModel( 'vendor', 'VirtuemartModel' );
-//		$view->setModel( $vendor, true );
-//
-//		$view->setLayout('mailregisteruser');
-//
-//		$ok=true;
-//		/* Render it all */
-//		ob_start();
-//		$view->display();
-//		$bodyShopper = ob_get_contents();
-//		ob_end_clean();
-//		$sendShopper = shopFunctionsF::sendMail($bodyShopper,$user->email);
-//		if ( $sendShopper !== true ) {
-//			$ok=false;
-//			//TODO set message, must be a raising one
-//		}
-//
-//		$view->setLayout('mailregistervendor');
-//
-//		/* Render it all */
-//		ob_start();
-//		$view->display();
-//		$bodyVendor = ob_get_contents();
-//		ob_end_clean();
-//
-//		$vendor->setId(1);  //TODO MaX at the moment is the new registered email for the vendor always send to the main store
-////		$vendor=$vendor->getVendor(true);
-//		$vendorData = $user->getUser($vendor->getUserIdByVendorId());
-//		$sendVendor = shopFunctionsF::sendMail($bodyVendor,$vendorData->jUser->email); //TODO MX set vendorId
-//		if ( $sendShopper !== true ) {
-//			$ok=false;
-//			//TODO set message, must be a raising one
-//		}
-//
-//
-//		//Just for developing
-//		echo '<br />$bodyUser '.$bodyShopper;
-//		echo '<br />$bodyVendor '.$bodyVendor;
-//		return $ok;
-//	}
+	* Editing a user address was cancelled during chaeckout; return to the cart
+	*
+	* @author Oscar van Eijk
+	*/
+	function cancelCheckoutUser(){
+		$this->setRedirect( JRoute::_('index.php?option=com_virtuemart&view=cart&task=checkout',$this->useXHTML,$this->useSSL), $msg );
+	}
 
 	/**
 	 * Action cancelled; return to the previous view
@@ -414,5 +279,7 @@ class VirtueMartControllerUser extends JController
 		$return = JURI::base();
 		$this->setRedirect( $return );
 	}
+
+
 }
 // No closing tag
