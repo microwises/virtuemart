e67e70d17227ce3f625ab61023d344ff82f56a74
enytheme <enytheme@87cc427b-4a1e-0410-b729-b2260ec0f063>
2011-10-07 06:40:20 +0000

    new manufacturer details view
    git-svn-id: https://dev.virtuemart.net/svn/virtuemart/trunk/virtuemart@4292 87cc427b-4a1e-0410-b729-b2260ec0f063

--- components/com_virtuemart/views/manufacturer/tmpl/details.php
+++ components/com_virtuemart/views/manufacturer/tmpl/details.php
@@ -5,7 +5,7 @@
 *
 * @package	VirtueMart
 * @subpackage Manufacturer
-* @author Kohl Patrick
+* @author Kohl Patrick, Eugen Stranz
 * @link http://www.virtuemart.net
 * @copyright Copyright (c) 2004 - 2010 VirtueMart Team. All rights reserved.
 * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.php
@@ -18,33 +18,51 @@
 
 // Check to ensure this file is included in Joomla!
 defined('_JEXEC') or die('Restricted access');
-?>
-
+$manufacturerImage = $this->manufacturer->images[0]->displayMediaThumb('class="manufacturer-image"',false);
+	$manufacturerProductsURL = JROUTE::_('index.php?option=com_virtuemart&view=category&search=true&virtuemart_manufacturer_id=' . $this->manufacturer->virtuemart_manufacturer_id);
+	?>
+	
 <div class="manufacturer-details-view">
+	<h1><?php echo $this->manufacturer->mf_name; ?></h1>
+	
 	<div class="spacer">
-
-		<?php // Manufacturer Image
-		echo $this->manufacturer->images[0]->displayMediaThumb('',false);?>
-
-		<h1><?php echo $this->manufacturer->mf_name; ?></h1>
-
-		<?php if(!empty($this->manufacturer->mf_email) || !empty($this->manufacturer->mf_url)) { ?>
-		<div class="email-weblink">
-		<?php
-		// TO DO Make The Email Visible Within The Lightbox
-		// echo JHtml::_('email.cloak', $this->manufacturer->mf_email,true,JText::_('COM_VIRTUEMART_EMAIL'),false) ?>
-		<a href="<?php echo $this->manufacturer->mf_url ?>"><?php echo JText::_('COM_VIRTUEMART_MANUFACTURER_PAGE') ?></a>
+	
+	<?php // Manufacturer Image
+	if (!empty($manufacturerImage)) { ?>
+		<div class="manufacturer-image">
+		<?php echo $manufacturerImage; ?>
 		</div>
-		<?php } ?>
-
-		<?php if(!empty($this->manufacturer->mf_desc)) { ?>
-			<div class="description">
-			<?php echo $this->manufacturer->mf_desc; ?>
-			</div>
-			<?php
-		}
-		$ProductCategorylink = JROUTE::_('index.php?option=com_virtuemart&view=category&search=true&virtuemart_manufacturer_id=' . $this->manufacturer->virtuemart_manufacturer_id);
-		?>
-		<a href="<?php echo $ProductCategorylink; ?>"><?php echo JText::sprintf('COM_VIRTUEMART_PRODUCT_FROM_MF',$this->manufacturer->mf_name); ?></a>
+	<?php } ?>
+	
+	<?php // Manufacturer Email
+	if(!empty($this->manufacturer->mf_email)) { ?>
+		<div class="manufacturer-email">
+		<?php // TO DO Make The Email Visible Within The Lightbox
+		echo JHtml::_('email.cloak', $this->manufacturer->mf_email,true,JText::_('COM_VIRTUEMART_EMAIL'),false) ?>
+		</div>
+	<?php } ?>
+	
+	<?php // Manufacturer URL
+	if(!empty($this->manufacturer->mf_url)) { ?>
+		<div class="manufacturer-url">
+			<a href="<?php echo $this->manufacturer->mf_url ?>"><?php echo JText::_('COM_VIRTUEMART_MANUFACTURER_PAGE') ?></a>
+		</div>
+	<?php } ?>
+	
+	<?php // Manufacturer Description
+	if(!empty($this->manufacturer->mf_desc)) { ?>
+		<div class="manufacturer-description">
+			<?php echo $this->manufacturer->mf_desc ?>
+		</div>
+	<?php } ?>
+	
+	<?php // Manufacturer Product Link
+	if(!empty($manufacturerProductsURL)) { ?>
+		<div class="manufacturer-product-link">
+			<a href="<?php echo $manufacturerProductsURL; ?>"><?php echo JText::sprintf('COM_VIRTUEMART_PRODUCT_FROM_MF',$this->manufacturer->mf_name); ?></a>
+		</div>
+	<?php } ?>
+	
+	<div class="clear"></div>
 	</div>
 </div>
