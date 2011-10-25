cddc0954a7800a5db40d271c240bdafd90726b4a
grtrustme <grtrustme@87cc427b-4a1e-0410-b729-b2260ec0f063>
2011-10-25 18:00:45 +0000

    A typo and some aisthetic stuff
    git-svn-id: https://dev.virtuemart.net/svn/virtuemart/trunk/virtuemart@4548 87cc427b-4a1e-0410-b729-b2260ec0f063

--- modules/mod_virtuemart_product/helper.php
+++ modules/mod_virtuemart_product/helper.php
@@ -15,11 +15,8 @@ if( !defined( '_VALID_MOS' ) && !defined( '_JEXEC' ) ) die( 'Direct Access to '.
 */
 if (!class_exists( 'VmConfig' )) require(JPATH_ADMINISTRATOR . DS . 'components' . DS . 'com_virtuemart'.DS.'helpers'.DS.'config.php');
 VmConfig::loadConfig();
-
-// Load the language file of com_virtuemart. In many cases the end user will need it
-$lang = JFactory::getLanguage();
-$lang->load('com_virtuemart');
-
+// Load the language file of com_virtuemart.
+JFactory::getLanguage()->load('com_virtuemart');
 if (!class_exists( 'calculationHelper' )) require(JPATH_ADMINISTRATOR . DS . 'components' . DS . 'com_virtuemart'.DS.'helpers'.DS.'calculationh.php');
 if (!class_exists( 'CurrencyDisplay' )) require(JPATH_ADMINISTRATOR . DS . 'components' . DS . 'com_virtuemart'.DS.'helpers'.DS.'currencydisplay.php');
 if (!class_exists( 'VirtueMartModelVendor' )) require(JPATH_ADMINISTRATOR . DS . 'components' . DS . 'com_virtuemart'.DS.'models'.DS.'vendor.php');
@@ -31,41 +28,46 @@ if (!class_exists( 'VirtueMartModelProduct' )){
 }
 
 class mod_virtuemart_product {
+    
 	function addtocart($product) {
-	if (!VmConfig::get('use_as_catalog',0)) { ?>
-				<div class="addtocart-area">
-					<form method="post" class="product" action="index.php" id="addtocartproduct<?php echo $product->virtuemart_product_id ?>">
-	<?php // Product custom_fields
-	if (!empty($product->customfieldsCart)) {  ?>
-	<div class="product-fields">
-		<?php foreach ($product->customfieldsCart as $field)
-		{ ?><div style="display:inline-block;" class="product-field product-field-type-<?php echo $field->field_type ?>">
-			<span class="product-fields-title" ><b><?php echo $field->custom_title ?></b><span>
-			<?php echo JHTML::tooltip($field->custom_tip, $field->custom_title, 'tooltip.png'); ?>
-			<span class="product-field-display"><?php echo $field->display ?><span>
+            if (!VmConfig::get('use_as_catalog',0)) { ?>
+                <div class="addtocart-area">
+                    
+		<form method="post" class="product" action="index.php" id="addtocartproduct<?php echo $product->virtuemart_product_id ?>">
+                    <?php 
+                    // Product custom_fields
+                    if (!empty($product->customfieldsCart)) {  ?>
+                        <div class="product-fields">
+                        <?php foreach ($product->customfieldsCart as $field) { ?>
+                        
+                            <div style="display:inline-block;" class="product-field product-field-type-<?php echo $field->field_type ?>">
+                            <span class="product-fields-title" ><b><?php echo $field->custom_title ?></b><span>
+                            <?php echo JHTML::tooltip($field->custom_tip, $field->custom_title, 'tooltip.png'); ?>
+                            <span class="product-field-display"><?php echo $field->display ?><span>
+                            <span class="product-field-desc"><?php echo $field->custom_field_desc ?><span>
+                            </div>
+                        
+                        <?php } ?>
+                        </div>
+                    <?php } ?>
 
-			<span class="product-field-desc"><?php echo $field->custom_field_desc ?><span>
-			</div>
-			<?php
-		}
-		?>
-	</div>
-	<?php } ?>
+                    <div class="addtocart-bar">
 
-		<div class="addtocart-bar">
-
-			<?php // Display the quantity box ?>
+			<?php 
+                        // Display the quantity box 
+                        ?>
 			<!-- <label for="quantity<?php echo $product->virtuemart_product_id;?>" class="quantity_box"><?php echo JText::_('COM_VIRTUEMART_CART_QUANTITY'); ?>: </label> -->
 			<span class="quantity-box">
-				<input type="text" class="quantity-input" name="quantity[]" value="1" />
+			<input type="text" class="quantity-input" name="quantity[]" value="1" />
 			</span>
 			<span class="quantity-controls">
-				<input type="button" class="quantity-controls quantity-plus" />
-				<input type="button" class="quantity-controls quantity-minus" />
+			<input type="button" class="quantity-controls quantity-plus" />
+			<input type="button" class="quantity-controls quantity-minus" />
 			</span>
-			<?php // Display the quantity box END ?>
+			
 
-			<?php // Add the button
+			<?php 
+                        // Add the button
 			$button_lbl = JText::_('COM_VIRTUEMART_CART_ADD_TO');
 			$button_cls = ''; //$button_cls = 'addtocart_button';
 			if (VmConfig::get('check_stock') == '1' && !$product->product_in_stock) {
@@ -78,22 +80,20 @@ class mod_virtuemart_product {
 				<input type="submit" name="addtocart"  class="addtocart-button" value="<?php echo $button_lbl ?>" title="<?php echo $button_lbl ?>" />
 			</span>
 
+                        <div class="clear"></div>
+                    </div>
+		
+                    <input type="hidden" class="pname" value="<?php echo $product->product_name ?>">
+                    <input type="hidden" name="option" value="com_virtuemart" />
+                    <input type="hidden" name="view" value="cart" />
+                    <noscript><input type="hidden" name="task" value="add" /></noscript>
+                    <input type="hidden" name="virtuemart_product_id[]" value="<?php echo $product->virtuemart_product_id ?>" />
+                    <?php /** @todo Handle the manufacturer view */ ?>
+                    <input type="hidden" name="virtuemart_manufacturer_id" value="<?php echo $product->virtuemart_manufacturer_id ?>" />
+                    <input type="hidden" name="virtuemart_category_id[]" value="<?php echo $product->virtuemart_category_id ?>" />
+                </form>
 		<div class="clear"></div>
-		</div>
-
-		<?php // Display the add to cart button END ?>
-		<input type="hidden" class="pname" value="<?php echo $product->product_name ?>">
-		<input type="hidden" name="option" value="com_virtuemart" />
-		<input type="hidden" name="view" value="cart" />
-		<noscript><input type="hidden" name="task" value="add" /></noscript>
-		<input type="hidden" name="virtuemart_product_id[]" value="<?php echo $product->virtuemart_product_id ?>" />
-		<?php /** @todo Handle the manufacturer view */ ?>
-		<input type="hidden" name="virtuemart_manufacturer_id" value="<?php echo $product->virtuemart_manufacturer_id ?>" />
-		<input type="hidden" name="virtuemart_category_id[]" value="<?php echo $product->virtuemart_category_id ?>" />
-	</form>
-
-				<div class="clear"></div>
-				</div>
-			<?php }
-	}
+            </div>
+        <?php }
+     } 
 }
