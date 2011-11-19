4296dce87b9cfaf486a4ff264e3fc003d83fa791
electrocity <electrocity@87cc427b-4a1e-0410-b729-b2260ec0f063>
2011-11-19 01:47:04 +0000

    Switch language in joomla 1.7
    git-svn-id: https://dev.virtuemart.net/svn/virtuemart/trunk/virtuemart@4769 87cc427b-4a1e-0410-b729-b2260ec0f063

--- administrator/components/com_virtuemart/helpers/config.php
+++ administrator/components/com_virtuemart/helpers/config.php
@@ -384,12 +384,20 @@ class VmConfig {
 			$siteLang = JRequest::getVar('vmlang',FALSE );// we must have this for edit form save
 			//Why not using the usterstae?
 		} else {
+			if ( self::isJ15() ) {
 			// try to find in session lang
 			// this work with joomfish j1.5 (application.data.lang)
-			// TODO test wiht j1.7
+
 			$session  =& JFactory::getSession();
 			$registry =& $session->get('registry');
-			if ( ! $siteLang = $registry->getValue('application.data.lang') ) {
+			$siteLang = $registry->getValue('application.data.lang') ;
+			} else  { 
+			// TODO test wiht j1.7
+			jimport('joomla.language.helper');
+			$languages = JLanguageHelper::getLanguages('lang_code');
+			$siteLang = JFactory::getLanguage()->getTag();
+			} 
+			if ( ! $siteLang ) {
 				// use user default
 				$lang =& JFactory::getLanguage();
 				$siteLang = $lang->getName();
