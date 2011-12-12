c7938e4c4e1625055aced009166723ad8e0d966b
alatak <alatak@87cc427b-4a1e-0410-b729-b2260ec0f063>
2011-12-12 21:38:07 +0000

    W3C compliance from Alex
    git-svn-id: https://dev.virtuemart.net/svn/virtuemart/trunk/virtuemart@5048 87cc427b-4a1e-0410-b729-b2260ec0f063

--- modules/mod_virtuemart_product/helper.php
+++ modules/mod_virtuemart_product/helper.php
@@ -28,33 +28,33 @@ if (!class_exists( 'VirtueMartModelProduct' )){
 }
 
 class mod_virtuemart_product {
-    
+
 	function addtocart($product) {
             if (!VmConfig::get('use_as_catalog',0)) { ?>
                 <div class="addtocart-area">
-                    
+
 		<form method="post" class="product" action="index.php">
-                    <?php 
+                    <?php
                     // Product custom_fields
                     if (!empty($product->customfieldsCart)) {  ?>
                         <div class="product-fields">
                         <?php foreach ($product->customfieldsCart as $field) { ?>
-                        
+
                             <div style="display:inline-block;" class="product-field product-field-type-<?php echo $field->field_type ?>">
-                            <span class="product-fields-title" ><b><?php echo $field->custom_title ?></b><span>
+                            <span class="product-fields-title" ><b><?php echo $field->custom_title ?></b></span>
                             <?php echo JHTML::tooltip($field->custom_tip, $field->custom_title, 'tooltip.png'); ?>
-                            <span class="product-field-display"><?php echo $field->display ?><span>
-                            <span class="product-field-desc"><?php echo $field->custom_field_desc ?><span>
+                            <span class="product-field-display"><?php echo $field->display ?></span>
+                            <span class="product-field-desc"><?php echo $field->custom_field_desc ?></span>
                             </div>
-                        
+
                         <?php } ?>
                         </div>
                     <?php } ?>
 
                     <div class="addtocart-bar">
 
-			<?php 
-                        // Display the quantity box 
+			<?php
+                        // Display the quantity box
                         ?>
 			<!-- <label for="quantity<?php echo $product->virtuemart_product_id;?>" class="quantity_box"><?php echo JText::_('COM_VIRTUEMART_CART_QUANTITY'); ?>: </label> -->
 			<span class="quantity-box">
@@ -64,9 +64,9 @@ class mod_virtuemart_product {
 			<input type="button" class="quantity-controls quantity-plus" />
 			<input type="button" class="quantity-controls quantity-minus" />
 			</span>
-			
 
-			<?php 
+
+			<?php
                         // Add the button
 			$button_lbl = JText::_('COM_VIRTUEMART_CART_ADD_TO');
 			$button_cls = ''; //$button_cls = 'addtocart_button';
@@ -82,8 +82,8 @@ class mod_virtuemart_product {
 
                         <div class="clear"></div>
                     </div>
-		
-                    <input type="hidden" class="pname" value="<?php echo $product->product_name ?>">
+
+                    <input type="hidden" class="pname" value="<?php echo $product->product_name ?>"/>
                     <input type="hidden" name="option" value="com_virtuemart" />
                     <input type="hidden" name="view" value="cart" />
                     <noscript><input type="hidden" name="task" value="add" /></noscript>
@@ -93,5 +93,5 @@ class mod_virtuemart_product {
 		<div class="clear"></div>
             </div>
         <?php }
-     } 
+     }
 }
