02c098f520c643fb726b081af4db6e2699cfe76a
AlexTampa <AlexTampa@87cc427b-4a1e-0410-b729-b2260ec0f063>
2011-12-30 17:19:16 +0000

    git-svn-id: https://dev.virtuemart.net/svn/virtuemart/trunk/virtuemart@5198 87cc427b-4a1e-0410-b729-b2260ec0f063

---
+++ modules/mod_virtuemart_product/tmpl/default3.php
@@ -0,0 +1,88 @@
+<?php // no direct access
+defined('_JEXEC') or die('Restricted access');
+$col= 1 ;
+$pwidth= ' width'.floor ( 100 / $products_per_row );
+if ($products_per_row > 1) { $float= "floatleft";}
+else {$float="center";}
+?>
+<div class="vmgroup<?php echo $params->get( 'moduleclass_sfx' ) ?>">
+<?php if ($headerText) { ?>
+	<div class="vmheader"><?php echo $headerText ?></div>
+<?php }
+if ($display_style =="div") { ?>
+<div class="vmproduct<?php echo $params->get('moduleclass_sfx'); ?>">
+<?php foreach ($products as $product) { ?>
+	<div class="<?php echo $pwidth ?> <?php echo $float ?>"><div class="spacer">
+<?php
+ if (!empty($product->images[0]) )
+ $image = $product->images[0]->displayMediaThumb('class="featuredProductImage" border="0"',false) ;
+ else $image = '';
+ echo JHTML::_('link', JRoute::_('index.php?option=com_virtuemart&view=productdetails&virtuemart_product_id='.$product->virtuemart_product_id.'&virtuemart_category_id='.$product->virtuemart_category_id),$image,array('title' => $product->product_name) );
+ echo '<div class="clear"></div>';
+ $url = JRoute::_('index.php?option=com_virtuemart&view=productdetails&virtuemart_product_id='.$product->virtuemart_product_id.'&virtuemart_category_id='.
+$product->virtuemart_category_id); ?>		<a href="<?php echo $url ?>"><?php echo $product->product_name ?></a>		<?php	echo '<div class="clear"></div>';
+
+ if ($show_price) {
+ // 		echo $currency->priceDisplay($product->prices['salesPrice']);
+ if (!empty($product->prices['salesPrice'] ) ) echo $currency->createPriceDiv('salesPrice','',$product->prices,true);
+ // 		if ($product->prices['salesPriceWithDiscount']>0) echo $currency->priceDisplay($product->prices['salesPriceWithDiscount']);
+ if (!empty($product->prices['salesPriceWithDiscount']) ) echo $currency->createPriceDiv('salesPriceWithDiscount','',$product->prices,true);
+ }
+ if ($show_addtocart) echo mod_virtuemart_product::addtocart($product);
+ ?>
+ </div></div>
+	<?php
+		if ($col == $products_per_row && $products_per_row && $col < $totalProd ) {
+			echo "	</div><div style='clear:both;'>";
+			$col= 1 ;
+		} else {
+			$col++;
+		}
+	} ?>
+</div>
+<br style='clear:both;' />
+
+<?php
+} else {
+$last = count($products)-1;
+?>
+
+<ul class="vmproduct<?php echo $params->get('moduleclass_sfx'); ?>">
+<?php foreach ($products as $product) : ?>
+ <li class="<?php echo $pwidth ?> <?php echo $float ?>">
+ <?php
+ if (!empty($product->images[0]) )
+			$image = $product->images[0]->displayMediaThumb('class="featuredProductImage" border="0"',false) ;
+		else $image = '';
+			echo JHTML::_('link', JRoute::_('index.php?option=com_virtuemart&view=productdetails&virtuemart_product_id='.$product->virtuemart_product_id.'&virtuemart_category_id='.$product->virtuemart_category_id),$image,array('title' => $product->product_name) );
+			echo '<div class="clear"></div>';
+	$url = JRoute::_('index.php?option=com_virtuemart&view=productdetails&virtuemart_product_id='.$product->virtuemart_product_id.'&virtuemart_category_id='.
+$product->virtuemart_category_id); ?>		<a href="<?php echo $url ?>"><?php echo $product->product_name ?></a>		<?php	echo '<div class="clear"></div>';
+
+		if ($show_price) {
+			echo $currency->createPriceDiv('salesPrice','',$product->prices,true);
+			if ($product->prices['salesPriceWithDiscount']>0) echo $currency->createPriceDiv('salesPriceWithDiscount','',$product->prices,true);
+		}
+		if ($show_addtocart) echo mod_virtuemart_product::addtocart($product);
+		?>
+	</li>
+<?php
+	if ($col == $products_per_row && $products_per_row && $last ) {
+		echo '
+		</ul><div class="clear"></div>
+		<ul  class="vmproduct'.$params->get('moduleclass_sfx')  .'">';
+		$col= 1 ;
+	} else {
+		$col++;
+	}
+	$last--;
+	endforeach; ?>
+</ul><div class="clear"></div>
+
+<?php }
+	if ($footerText) : ?>
+	<div class="vmfooter<?php echo $params->get( 'moduleclass_sfx' ) ?>">
+		 <?php echo $footerText ?>
+	</div>
+<?php endif; ?>
+</div>
