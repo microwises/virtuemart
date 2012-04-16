5ffac9d05a58823b413d1e54c88c11e42a8fb10f
alatak <alatak@87cc427b-4a1e-0410-b729-b2260ec0f063>
2012-04-16 16:44:28 +0000

    typo
    git-svn-id: https://dev.virtuemart.net/svn/virtuemart/trunk/virtuemart@5917 87cc427b-4a1e-0410-b729-b2260ec0f063

--- components/com_virtuemart/views/invoice/tmpl/invoice_items.php
+++ components/com_virtuemart/views/invoice/tmpl/invoice_items.php
@@ -172,7 +172,7 @@ if ($this->orderDetails['details']['BT']->coupon_discount <> 0.00) {
 	</tr>
 
 <tr>
-		<td align="right" class="pricePad" colspan="6<?php echo $this->orderDetails['paymentName']  ?><td>
+		<td align="right" class="pricePad" colspan="6"><?php echo $this->orderDetails['paymentName']  ?><td>
 
 			<?php if ( VmConfig::get('show_tax')) { ?>
 				<td align="right"><?php echo "<span  class='priceColor2'>".$this->currency->priceDisplay($this->orderDetails['details']['BT']->order_payment_tax)."</span>" ?></td>
