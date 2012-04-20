ea9aee4f98d7b6977872b23f8d1dba0666f4706c
alatak <alatak@87cc427b-4a1e-0410-b729-b2260ec0f063>
2012-04-20 13:47:05 +0000

    missing file from klarna
    git-svn-id: https://dev.virtuemart.net/svn/virtuemart/trunk/virtuemart@5932 87cc427b-4a1e-0410-b729-b2260ec0f063

---
+++ plugins/vmpayment/klarna/klarna/helpers/define.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ *
+ * a special type of Klarna
+ * @author Val√©rie Isaksen
+ * @version $Id:
+ * @package VirtueMart
+ * @subpackage payment
+ * @copyright Copyright (C) 2004-2008 soeren - All rights reserved.
+ * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.php
+ * VirtueMart is free software. This version may have been modified pursuant
+ * to the GNU General Public License, and as distributed it includes or
+ * is derivative of works licensed under the GNU General Public License or
+ * other free or open source software licenses.
+ * See /administrator/components/com_virtuemart/COPYRIGHT.php for copyright notices and details.
+ *
+ * http://virtuemart.net
+ */
+defined('JPATH_BASE') or die();
+if (JVM_VERSION === 2) {
+    if (!defined('JPATH_VMKLARNAPLUGIN'))
+	define('JPATH_VMKLARNAPLUGIN', JPATH_ROOT . DS . 'plugins' . DS . 'vmpayment' . DS . 'klarna');
+    if (!defined('VMKLARNAPLUGINWEBROOT'))
+	define('VMKLARNAPLUGINWEBROOT', 'plugins/vmpayment/klarna');
+    if (!defined('VMKLARNAPLUGINWEBASSETS'))
+	define('VMKLARNAPLUGINWEBASSETS', JURI::root() . VMKLARNAPLUGINWEBROOT . '/klarna/assets');
+    if (!defined('VMKLARNA_PC_URI'))
+	define('VMKLARNA_PC_URI', JPATH_VMKLARNAPLUGIN . '/klarna/elements/klarna.json');
+} else {
+    if (!defined('JPATH_VMKLARNAPLUGIN'))
+	define('JPATH_VMKLARNAPLUGIN', JPATH_ROOT . DS . 'plugins' . DS . 'vmpayment');
+    if (!defined('VMKLARNAPLUGINWEBROOT'))
+	define('VMKLARNAPLUGINWEBROOT', 'plugins/vmpayment');
+    if (!defined('VMKLARNAPLUGINWEBASSETS'))
+	define('VMKLARNAPLUGINWEBASSETS', JURI::root() . VMKLARNAPLUGINWEBROOT . '/klarna/assets');
+    if (!defined('VMKLARNA_PC_URI'))
+	define('VMKLARNA_PC_URI', JPATH_VMKLARNAPLUGIN . '/klarna/elements/klarna.json');
+}
+if (!defined('VMKLARNA_PC_TYPE'))
+    define('VMKLARNA_PC_TYPE', 'json');
+?>
