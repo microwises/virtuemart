086a6183012436fd0b7788633cf05297917ce425
electrocity <electrocity@87cc427b-4a1e-0410-b729-b2260ec0f063>
2012-05-01 16:52:52 +0000

    Mail form Sendind code for user list
    git-svn-id: https://dev.virtuemart.net/svn/virtuemart/trunk/virtuemart@5978 87cc427b-4a1e-0410-b729-b2260ec0f063

--- administrator/components/com_virtuemart/views/product/tmpl/product_edit_customer.php
+++ administrator/components/com_virtuemart/views/product/tmpl/product_edit_customer.php
@@ -19,25 +19,36 @@
 defined('_JEXEC') or die('Restricted access');
 
  ?>
+<div class="mailing">
+<input type="text" class="mail-subject" size="40" value ="<?php echo jText::sprintf('COM_VIRTUEMART_PRODUCT_EMAIL_SHOPPERS_SUBJECT',$this->product->product_name) ?>">
+<div class="button2-left" data-type="sendmail" >
+	<div class="blank" style="padding:0 6px;cursor: pointer;" title="<?php echo jText::_('COM_VIRTUEMART_PRODUCT_EMAIL_SEND_TIP'); ?>">
+		<span class="vmicon vmicon-16-email" ></span>
+		<?php echo Jtext::_('COM_VIRTUEMART_PRODUCT_EMAIL_SEND'); ?>
+	</div>
+</div>
+<br />
+<div><?php echo Jtext::_('COM_VIRTUEMART_PRODUCT_EMAIL_CONTENT') ?></div>
+<textarea style="width: 100%;" class="inputbox" id="mail-body" cols="35" rows="10"></textarea>
+<br />
+
 <span ID="customer-list-BT">
-<div class="button2-left" data-type="reserved">
-	<div class="blank" style="padding:0 6px;cursor: pointer;">
-		<div class="vmicon vmicon-16-forward" ></div>
+<div class="button2-left" data-type="reserved" >
+	<div class="blank" style="padding:0 6px;cursor: pointer;" title="<?php echo jText::_('COM_VIRTUEMART_PRODUCT_LIST_BOOKED_BUYER_TIP'); ?>">
+		<span class="vmicon vmicon-16-forward" ></span>
 		<?php echo Jtext::_('COM_VIRTUEMART_PRODUCT_LIST_BOOKED_BUYER'); ?>
 	</div>
 </div>
-<div class="button2-left" data-type="all">
-	<div class="blank" style="padding:0 6px;cursor: pointer;">
-		<div class="vmicon vmicon-16-forward-off" ></div>
+<div class="button2-left" data-type="all" >
+	<div class="blank" style="padding:0 6px;cursor: pointer;" title="<?php echo Jtext::_('COM_VIRTUEMART_PRODUCT_LIST_BOOKED_ALL_TIP'); ?>">
+		<span class="vmicon vmicon-16-forward-off" ></span>
 		<?php echo Jtext::_('COM_VIRTUEMART_PRODUCT_LIST_ALL_BUYER'); ?>
-
 	</div>
 </div>
-<div class="button2-left" data-type="delivered">
-	<div class="blank" style="padding:0 6px;cursor: pointer;">
-		<div class="vmicon vmicon-16-forward-off" ></div>
+<div class="button2-left" data-type="delivered" >
+	<div class="blank" style="padding:0 6px;cursor: pointer;" title="<?php echo Jtext::_('COM_VIRTUEMART_PRODUCT_LIST_DELIVERED_BUYER_TIP'); ?>" >
+		<span class="vmicon vmicon-16-forward-off" ></span>
 		<?php echo Jtext::_('COM_VIRTUEMART_PRODUCT_LIST_DELIVERED_BUYER'); ?>
-
 	</div>
 </div>
 </span>
@@ -52,7 +63,7 @@ defined('_JEXEC') or die('Restricted access');
 	</thead>
 	<tbody id="customers-list">
 	<?php
-	foreach ($this->customers as $virtuemart_order_user_id => $customer) { 
+	foreach ($this->customers as $virtuemart_order_user_id => $customer) {
 		?>
 			<tr class="customer" data-cid="<?php echo $virtuemart_order_user_id ?>">
 				<td class=''><?php echo $customer['customer_name'] ?></td>
@@ -65,21 +76,31 @@ defined('_JEXEC') or die('Restricted access');
 	?>
 	</tbody>
 </table>
+<div>
 <script type="text/javascript">
 <!--
 
 /* JS for list changes */
 var $customerListLink = '<?php echo 'index.php?option=com_virtuemart&view=product&format=json&type=userlist&virtuemart_product_id='.$this->product->virtuemart_product_id ?>';
 var $customerListtype='reserved';
-jQuery('#customer-list-BT .button2-left').click(function() {
+jQuery('.mailing .button2-left').click(function() {
 	//document.orderStatForm.task.value = 'updateOrderItemStatus';
 	that = jQuery(this).find('.vmicon');
 	if (that.hasClass('vmicon-16-forward-off')) 
 	{
 		that.removeClass('vmicon-16-forward-off').addClass('vmicon-16-forward');
 		jQuery(this).siblings().children().children().addClass('vmicon-16-forward-off').removeClass('vmicon-16-forward');
-		$type = jQuery(this).data('type');
-		jQuery.get($customerListLink,{ listType: $type },function(data){
+		$customerListtype = jQuery(this).data('type');
+		jQuery.getJSON($customerListLink,{ listType: $customerListtype },
+			function(data){
+				jQuery("#customers-list").html(data.value);
+		});
+	} else if(that.hasClass('vmicon-16-email')) {
+		var $subject = jQuery('.mailing .mail-subject').val();
+		var $body = jQuery('#mail-body').val();
+		
+		jQuery.getJSON($customerListLink,{ mailto: $customerListtype,subject: $subject,mailbody: $body, task: "mailing" },
+			function(data){
 				jQuery("#customers-list").html(data.value);
 		});
 	}
